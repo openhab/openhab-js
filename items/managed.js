7577f5089af33d1626743162477d58cec0672207
Dan Cunningham <dan@digitaldan.com>
2021-12-14 21:06:46 -0800

    Clean up docs and build script
    Signed-off-by: Dan Cunningham <dan@digitaldan.com>

--- items/managed.js
+++ items/managed.js
@@ -40,7 +40,7 @@ class Item {
 
         /**
          * Access historical states for this item
-         * @type {ItemHistory}
+         * @type {items.ItemHistory}
          */
         this.history = new ItemHistory(rawItem);
     }
@@ -407,7 +407,7 @@ const replaceItem = function (/* same args as addItem */) {
  * @memberOf items
  * @param {String} name the name of the item
  * @param {String} nullIfMissing whether to return null if the item cannot be found (default is to throw an exception)
- * @return {Item} the item
+ * @return {items.Item} the item
  */
 const getItem = (name, nullIfMissing = false) => {
     try {
@@ -428,7 +428,7 @@ const getItem = (name, nullIfMissing = false) => {
  * 
  * @memberOf items
  * @param {String[]} tagNames an array of tags to match against
- * @return {Item[]} the items with a tag that is included in the passed tags
+ * @return {items.Item[]} the items with a tag that is included in the passed tags
  */
 const getItemsByTag = (...tagNames) => {
     return utils.javaSetToJsArray(itemRegistry.getItemsByTag(tagNames)).map(i => new Item(i));
