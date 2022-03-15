97c7297d145f3490d900aa4a07506553029fe894
Richard Koshak <rlkoshak@gmail.com>
2022-03-15 12:15:18 -0600

    Removing remnants of bad merge
    Signed-off-by: Richard Koshak <rlkoshak@gmail.com>

--- time.js
+++ time.js
@@ -6,7 +6,7 @@ const time = require('@js-joda/core');
 const rfcFormatter = time.DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss.SSS[xxxx][xxxxx]");
 const targetParse = time.ZonedDateTime.prototype.parse;
 time.ZonedDateTime.prototype.parse = function (text, formatter = rfcFormatter) {
-  return targetParse(text, formatter);
+    return targetParse(text, formatter);
 }
 
 module.exports = time;
