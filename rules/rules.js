094822ba5c3c7cbb352f3bec30db3ebe96ee0a86
Florian Hotze <florianh_dev@icloud.com>
2022-05-17 17:09:27 +0200

    Rules: Failed rule run logs a useful error message (#116)
    * Rules: Failed rule run logs a meaningful error message before rethrow
    
    Log the JavaScript error instead of throwing an error that is caught by
    openHAB core.
    This logs the JavaScript error and not the unusable 'Failed to execute
    action : 1'.
    
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

--- rules/rules.js
+++ rules/rules.js
@@ -207,11 +207,20 @@ let JSRule = function (ruleConfig) {
     let SimpleRule = Java.extend(Java.type('org.openhab.core.automation.module.script.rulesupport.shared.simple.SimpleRule'));
 
     let doExecute = function (module, input) {
-        try {
-            return ruleConfig.execute(getTriggeredData(input));
-        } catch (error) {
-            throw Error(`Failed to execute rule ${ruid}: ${error}: ${error.stack}`);
+      try {
+        return ruleConfig.execute(getTriggeredData(input));
+      } catch (error) {
+        // logging error is required for meaningful error log message
+        // when throwing error: error is caught by core framework and no meaningful message is logged
+        let msg;
+        if (error.stack) {
+          msg = `Failed to execute rule ${ruid}: ${error}: ${error.stack}`;
+        } else {
+          msg = `Failed to execute rule ${ruid}: ${error}`;
         }
+        console.error(msg);
+        throw Error(msg);
+      }
     };
 
     let rule = new SimpleRule({
