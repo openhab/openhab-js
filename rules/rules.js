openhab/openhab-js

commit 5b310b00e0bafd41bb9a418ea5c0701a853e9d98

[rules] Improvements for event object & JSRule callback (#125)
* [rules] Access to raw input in JSRule callback

Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

* [rules] Make all properties of event object string

Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

* [rules] Document the event object & JSRule callback with JSDoc

Fixes [Docs] File Based Rules/Event Object types are incorrect #124.

Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

* [rules] Add link for event object JSDoc to README

Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

* [rules] Remove raw event data from `JSRule`s callback

Instead add `command` from PWM trigger to the event object.

Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

rules/rules.js
@@ -5,6 +5,33 @@
  * @namespace rules
  */
 
+/**
+ * @typedef {Object} EventObject When a rule is triggered, the script is provided the event instance that triggered it. The specific data depends on the event type. The `EventObject` provides several information about that trigger.
+ *
+ * Note:
+ * `ThingStatusUpdateTrigger`, `ThingStatusChangeTrigger` use *Thing* and `ChannelEventTrigger` uses the the trigger channel name as value for `itemName`.
+ * `Group****Trigger`s use the equivalent `Item****Trigger` as trigger for each member.
+ *
+ * @memberof rules
+ * @property {String} oldState only for {@link triggers.ItemStateChangeTrigger} & {@link triggers.GroupStateChangeTrigger}: Previous state of Item or Group that triggered event
+ * @property {String} newState only for {@link triggers.ItemStateChangeTrigger} & {@link triggers.GroupStateChangeTrigger}: New state of Item or Group that triggered event
+ * @property {String} state only for {@link triggers.ItemStateUpdateTrigger} & {@link triggers.GroupStateUpdateTrigger}: State of Item that triggered event
+ * @property {String} receivedCommand only for {@link triggers.ItemCommandTrigger} & {@link triggers.GroupCommandTrigger}: Command that triggered event
+ * @property {String} receivedState only for {@link triggers.ItemStateUpdateTrigger} & {@link triggers.GroupStateUpdateTrigger}: State that triggered event
+ * @property {*} receivedTrigger only for {@link triggers.ChannelEventTrigger}: Trigger that triggered event
+ * @property {String} itemName for all triggers except {@link triggers.PWMTrigger}: name of Item that triggered event
+ * @property {String} eventType for all triggers except `ThingStatus****Triggers`, {@link triggers.PWMTrigger}: Type of event that triggered event (change, command, time, triggered, update)
+ * @property {String} triggerType for all triggers except `ThingStatus****Triggers`, {@link triggers.PWMTrigger}: Type of trigger that triggered event (for `TimeOfDayTrigger`: `GenericCronTrigger`)
+ * @property {*} payload not for all triggers
+ * @property {String} command only for {@link triggers.PWMTrigger}: Pulse Width Modulation Automation command
+ */
+
+/**
+ * @callback RuleCallback When a rule is run, a callback is executed.
+ * @memberof rules
+ * @param {rules.EventObject} event
+ */
+
 const GENERATED_RULE_ITEM_TAG = 'GENERATED_RULE_ITEM';
 
 const items = require('../items');
@@ -178,14 +205,14 @@ const setEnabled = function (uid, isEnabled) {
   *  name: "my_new_rule",
   *  description: "this rule swizzles the swallows",
   *  triggers: triggers.GenericCronTrigger("0 30 16 * * ? *"),
-  *  execute: triggerConfig => { //do stuff }
+  *  execute: (event) => { // do stuff }
   * });
   *
   * @memberOf rules
   * @param {Object} ruleConfig The rule config describing the rule
   * @param {String} ruleConfig.name the name of the rule
   * @param {String} [ruleConfig.description] a description of the rule
-  * @param {*} ruleConfig.execute callback that will be called when the rule fires
+  * @param {rules.RuleCallback} ruleConfig.execute callback that will be called when the rule fires
   * @param {HostTrigger|HostTrigger[]} ruleConfig.triggers triggers which will define when to fire the rule
   * @param {String} [ruleConfig.id] the UID of the rule
   * @param {Array<String>} [ruleConfig.tags] the tags for the rule
@@ -300,7 +327,7 @@ const registerRule = function (rule) {
   * @param {Object} ruleConfig The rule config describing the rule
   * @param {String} ruleConfig.name the name of the rule
   * @param {String} [ruleConfig.description] a description of the rule
-  * @param {*} ruleConfig.execute callback that will be called when the rule fires
+  * @param {rules.RuleCallback} ruleConfig.execute callback that will be called when the rule fires
   * @param {HostTrigger|HostTrigger[]} ruleConfig.triggers triggers which will define when to fire the rule
   * @param {String} [ruleConfig.id] the UID of the rule
   * @param {Array<String>} [ruleConfig.tags] the tags for the rule
@@ -351,10 +378,10 @@ const getTriggeredData = function (input) {
     return {
       eventType: 'command',
       triggerType: 'ItemCommandTrigger',
-      receivedCommand: event.getItemCommand(),
+      receivedCommand: event.getItemCommand().toString(),
       oldState: input.get('oldState') + '',
       newState: input.get('newState') + '',
-      itemName: event.getItemName(),
+      itemName: event.getItemName().toString(),
       module: input.get('module')
     };
   }
@@ -377,8 +404,9 @@ const getTriggeredData = function (input) {
     receivedCommand: null,
     receivedState: null,
     receivedTrigger: null,
-    itemName: evArr[0],
-    module: input.get('module')
+    itemName: evArr[0].toString(),
+    module: input.get('module'),
+    command: input.get('command') + '' // for PWM trigger
   };
 
   try {

commit fa7b3bcd077dd1af8b8c91a6e36ba57812e92d8f

[rules] Fix EventObject.itemName `null` is string instead of null
Fixes an issue that I introduced in #125.

Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

rules/rules.js
@@ -381,7 +381,7 @@ const getTriggeredData = function (input) {
       receivedCommand: event.getItemCommand().toString(),
       oldState: input.get('oldState') + '',
       newState: input.get('newState') + '',
-      itemName: event.getItemName().toString(),
+      itemName: event.getItemName(),
       module: input.get('module')
     };
   }
@@ -404,7 +404,7 @@ const getTriggeredData = function (input) {
     receivedCommand: null,
     receivedState: null,
     receivedTrigger: null,
-    itemName: evArr[0].toString(),
+    itemName: evArr[0],
     module: input.get('module'),
     command: input.get('command') + '' // for PWM trigger
   };