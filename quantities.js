274b27bba605a362c0e628814d83372a79b134e4
Florian Hotze <florianh_dev@icloud.com>
2022-12-21 13:46:00 +0100

    [quantities] Polyfill support for additional unit notations
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

--- quantities.js
+++ quantities.js
@@ -1,18 +1,25 @@
 const Qty = require('js-quantities');
 
+// Polyfill support for additional unit notations
 // Use absolute temperature units here to allow conversion of measured temperatures
 const replaceUnits = (value) => {
-    if (typeof value === 'string') {
-        return value
-            .replace(/(°|° )C/, 'tempC')
-            .replace(/(°|° )F/, 'tempF')
-            .replace('K', 'tempK')
-            .replace('°', 'degree');
-    } else {
-        return value;
-    }
+  if (typeof value === 'string') {
+    return value
+      .replace(/(°|° )C/, 'tempC')
+      .replace(/(°|° )F/, 'tempF')
+      .replace('K', 'tempK')
+      .replace('°', 'degree')
+      .replace('fur', 'furlong') // Imperial length unit
+      .replace('lea', 'league') // Imperial length unit
+      .replace('ɡₙ', 'gee') // Acceleration: Standard Gravity
+      .replace('Ws', 'W*s') // Energy: Watt Seconds
+      .replace(/VArh/i, 'VAr*h') // Energy: Volt-Ampere Reactive Hours
+      .replace('lx', 'lux') // Illuminance: Lux
+      .replace(/(dBm)(W)?/, 'dB*mW') // Power: Decibel-Milliwatts
+      .replace('VAh', 'VA*h'); // Power: Volt-Ampere Hours
+  } else {
+    return value;
+  }
 };
 
-// Polyfill support for temperature units with the degree sign ° with or without a space in between, e.g. °C or °F
-// Polyfill support for angle in degrees using the degree sign °
 module.exports = (initValue, initUnits) => Qty(replaceUnits(initValue), replaceUnits(initUnits));
