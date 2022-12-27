openhab/openhab-js

commit 5084f5524b19430d68a37f0b6385c8640e8a8e41

Fix & Improve TS type declarations (#209)
* Move Java types to global scope.
* Import ZonedDateTime type from @js-joda.
* Fix return types (remove prefixes).
* Import item type.
The type-reference to `items.Item` is not generated automatically by the
compiler, as it is not used in the (JavaScript) code. So, we import it
explicitly for typing.
* Add namespace-prefix to type.
* Add type-testing to npm scripts.
Test correct typings by running `tsc` compiler with `*.d.ts` files as
input.
* Fix type for optional rest parameters.
* Generate type definitions with previous changes.
* Export class Logger.
* Mark class properties as private.
* Use primitve types of object and string.
* Generate type definitions with previous changes.
* Type definitions: Rename Java globals
* [actions & items] Import type def for `time.ZonedDateTime`
* Regenerate type definitions
* [rules] Enable type definitions for Rule Builder
* Update CHANGELOG & DEPLOY
* Address review comment

Also-by: Florian Hotze florianh_dev@icloud.com
Signed-off-by: Stefan Friedle stefan@friedle.de

rules/index.js
@@ -1,8 +1,0 @@
-const rules = require('./rules');
-/**
- * @type {RuleBuilder}
- * @private
- */
-const builder = require('./rule-builder');
-
-module.exports = Object.assign(rules, builder);

rules/rules.js
@@ -454,5 +454,6 @@ module.exports = {
   isEnabled,
   setEnabled,
   JSRule,
-  SwitchableJSRule
+  SwitchableJSRule,
+  when: require('./rule-builder').when
 };

types/rules/index.d.ts
@@ -1,3 +1,0 @@
-declare const _exports: any;
-export = _exports;
-//# sourceMappingURL=index.d.ts.map

types/rules/index.d.ts.map
@@ -1,1 +1,0 @@
-{"version":3,"file":"index.d.ts","sourceRoot":"","sources":["../../rules/index.js"],"names":[],"mappings":""}

types/rules/rules.d.ts
@@ -170,4 +170,5 @@ export function JSRule(ruleConfig: RuleConfig): HostRule;
   */
 export function SwitchableJSRule(ruleConfig: RuleConfig): HostRule;
 import triggers = require("../triggers");
+export declare const when: (withToggle?: boolean) => import("./trigger-builder").TriggerBuilder;
 //# sourceMappingURL=rules.d.ts.map

index.js
@@ -1,21 +1,3 @@
-/**
- * @typedef {object} HostState Native Java openHAB State (instance of {@link https://www.openhab.org/javadoc/latest/org/openhab/core/types/state org.openhab.core.types.State})
- */
-/**
- * @typedef {object} HostItem Native Java openHAB Item (instance of {@link https://www.openhab.org/javadoc/latest/org/openhab/core/items/item org.openhab.core.items.Item})
- */
-/**
- * @typedef {object} HostClass Native Java Class Object (instance of java.lang.Class)
- */
-/**
- * @typedef {object} HostRule Native Jave openHAB Rule (instance of {@link https://www.openhab.org/javadoc/latest/org/openhab/core/automation/rule org.openhab.core.automation.Rule})
- */
-/**
- * @typedef {object} HostTrigger Native Jave openHAB Trigger (instance of {@link https://www.openhab.org/javadoc/latest/org/openhab/core/automation/trigger org.openhab.core.automation.Trigger})
- */
-/**
- * @typedef {object} HostThing Native Java openHAB Thing (instance of {@link https://www.openhab.org/javadoc/latest/org/openhab/core/thing/thing org.openhab.core.thing.Thing})
- */
 
 // lazy getters to avoid any reference loading all submodules
 module.exports = {

types/index.d.ts
@@ -1,25 +1,2 @@
-/**
- * Native Java openHAB State (instance of {@link https://www.openhab.org/javadoc/latest/org/openhab/core/types/state org.openhab.core.types.State})
- */
-export type HostState = object;
-/**
- * Native Java openHAB Item (instance of {@link https://www.openhab.org/javadoc/latest/org/openhab/core/items/item org.openhab.core.items.Item})
- */
-export type HostItem = object;
-/**
- * Native Java Class Object (instance of java.lang.Class)
- */
-export type HostClass = object;
-/**
- * Native Jave openHAB Rule (instance of {@link https://www.openhab.org/javadoc/latest/org/openhab/core/automation/rule org.openhab.core.automation.Rule})
- */
-export type HostRule = object;
-/**
- * Native Jave openHAB Trigger (instance of {@link https://www.openhab.org/javadoc/latest/org/openhab/core/automation/trigger org.openhab.core.automation.Trigger})
- */
-export type HostTrigger = object;
-/**
- * Native Java openHAB Thing (instance of {@link https://www.openhab.org/javadoc/latest/org/openhab/core/thing/thing org.openhab.core.thing.Thing})
- */
-export type HostThing = object;
+export {};
 //# sourceMappingURL=index.d.ts.map

types/index.d.ts.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"index.d.ts","sourceRoot":"","sources":["../index.js"],"names":[],"mappings":";;;wBACa,MAAM;;;;uBAGN,MAAM;;;;wBAGN,MAAM;;;;uBAGN,MAAM;;;;0BAGN,MAAM;;;;wBAGN,MAAM"}
+{"version":3,"file":"index.d.ts","sourceRoot":"","sources":["../index.js"],"names":[],"mappings":""}

commit 974c7031a8b88c720706e0c884f3e3893a705388

[rules] Fix rule builder (#210)
Fixes a regression from #209 that broke rule builder.

Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

rules/index.js
@@ -1,0 +1,4 @@
+module.exports = {
+  ...require('./rules'),
+  when: require('./rule-builder').when
+};

rules/rules.js
@@ -454,6 +454,5 @@ module.exports = {
   isEnabled,
   setEnabled,
   JSRule,
-  SwitchableJSRule,
-  when: require('./rule-builder').when
+  SwitchableJSRule
 };

types/rules/index.d.ts
@@ -1,0 +1,11 @@
+declare const _exports: {
+    when: (withToggle?: boolean) => import("./trigger-builder").TriggerBuilder;
+    removeRule: (uid: string) => boolean;
+    runRule: (uid: string, args?: any, cond?: boolean) => void;
+    isEnabled: (uid: string) => boolean;
+    setEnabled: (uid: string, isEnabled: boolean) => void;
+    JSRule: (ruleConfig: import("./rules").RuleConfig) => HostRule;
+    SwitchableJSRule: (ruleConfig: import("./rules").RuleConfig) => HostRule;
+};
+export = _exports;
+//# sourceMappingURL=index.d.ts.map

types/rules/index.d.ts.map
@@ -1,0 +1,1 @@
+{"version":3,"file":"index.d.ts","sourceRoot":"","sources":["../../rules/index.js"],"names":[],"mappings":""}

types/rules/rules.d.ts
@@ -170,5 +170,4 @@ export function JSRule(ruleConfig: RuleConfig): HostRule;
   */
 export function SwitchableJSRule(ruleConfig: RuleConfig): HostRule;
 import triggers = require("../triggers");
-export declare const when: (withToggle?: boolean) => import("./trigger-builder").TriggerBuilder;
 //# sourceMappingURL=rules.d.ts.map