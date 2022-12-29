2ceed28c687203000556e2a8c1563528ca463051
Florian Hotze <florianh_dev@icloud.com>
2022-12-29 19:19:36 +0100

    [items] Refactor ItemChannelLink
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

---
+++ items/metadata/itemchannellink.js
@@ -0,0 +1,138 @@
+/**
+ * Item channel link namespace.
+ * This namespace provides access to Item channel links.
+ *
+ * @namespace items.itemchannellink
+ */
+
+const osgi = require('../../osgi');
+const log = require('../../log')('itemchannellink');
+
+const ItemChannelLink = Java.type('org.openhab.core.thing.link.ItemChannelLink');
+const ChannelUID = Java.type('org.openhab.core.thing.ChannelUID');
+const Configuration = Java.type('org.openhab.core.config.core.Configuration');
+const managedItemChannelLinkProvider = osgi.getService('org.openhab.core.thing.link.ManagedItemChannelLinkProvider');
+
+/**
+ * Creates a new ItemChannelLink object.
+ *
+ * @private
+ * @param {string} itemName the name of the Item
+ * @param {string} channelUID
+ * @param {object} [conf] channel configuration
+ * @returns {ItemChannelLink} ItemChannelLink object
+ */
+const _createItemChannelLink = function (itemName, channelUID, conf) {
+  log.debug(`Creating ItemChannelLink ${itemName} -> ${channelUID}...`);
+  if (typeof conf === 'object') {
+    log.debug(`  .. with configuration ${conf}`);
+    return new ItemChannelLink(itemName, new ChannelUID(channelUID), new Configuration(conf));
+  } else {
+    return new ItemChannelLink(itemName, new ChannelUID(channelUID));
+  }
+};
+
+/**
+ * Gets an ItemChannelLink from the provider.
+ *
+ * @private
+ * @param {string} itemName the name of the Item
+ * @param {string} channelUID
+ * @returns {{itemName: string, configuration: *, channelUID: string}|null} the ItemChannelLink or `null` if none exists
+ */
+const getItemChannelLink = function (itemName, channelUID) {
+  log.debug(`Getting ItemChannelLink ${itemName} -> ${channelUID} from provider...`);
+  const itemChannelLink = managedItemChannelLinkProvider.get(itemName + ' -> ' + channelUID);
+  if (itemChannelLink === null) return null;
+  return {
+    itemName: itemChannelLink.itemName.toString(),
+    channelUID: itemChannelLink.channelUID.toString(),
+    configuration: itemChannelLink.configuration
+  };
+};
+
+/**
+ * Adds a new ItemChannelLink to the provider. Therefore, it adds a channel link to an Item.
+ *
+ * @private
+ * @param {string} itemName the name of the Item
+ * @param {string} channelUID
+ * @param {object} [conf] channel configuration
+ * @returns {{itemName: string, configuration: *, channelUID: string}} the ItemChannelLink
+ */
+const _addItemChannelLink = function (itemName, channelUID, conf) {
+  log.debug(`Adding ItemChannelLink ${itemName} -> ${channelUID} to provider...`);
+  const itemChannelLink = _createItemChannelLink(itemName, channelUID, conf);
+  managedItemChannelLinkProvider.add(itemChannelLink);
+  return {
+    itemName: itemChannelLink.itemName.toString(),
+    channelUID: itemChannelLink.channelUID.toString(),
+    configuration: itemChannelLink.configuration
+  };
+};
+
+/**
+ * Update an ItemChannelLink in the provider. Therefore, it updates the channel link of an Item.
+ *
+ * @private
+ * @param {string} itemName the name of the Item
+ * @param {string} channelUID
+ * @param {object} [conf] channel configuration
+ * @returns {{itemName: string, configuration: *, channelUID: string}|null} the old ItemChannelLink or `null` if none exists
+ */
+const _updateItemChannelLink = function (itemName, channelUID, conf) {
+  log.debug(`Updating ItemChannelLink ${itemName} -> ${channelUID} in provider...`);
+  const itemChannelLink = _createItemChannelLink(itemName, channelUID, conf);
+  managedItemChannelLinkProvider.update(itemChannelLink);
+  if (itemChannelLink === null) return null;
+  return {
+    itemName: itemChannelLink.itemName.toString(),
+    channelUID: itemChannelLink.channelUID.toString(),
+    configuration: itemChannelLink.configuration
+  };
+};
+
+/**
+ * Adds or updates an ItemChannelLink.
+ *
+ * @memberof items.itemchannellink
+ * @param {string} itemName the name of the Item
+ * @param {string} channelUID
+ * @param {object} [conf] channel configuration
+ * @returns {boolean} `true` if the channel link was added, `false` if it was updated
+ */
+const replaceItemChannelLink = function (itemName, channelUID, conf) {
+  const existing = getItemChannelLink(itemName, channelUID);
+  if (existing === null) {
+    _addItemChannelLink(itemName, channelUID, conf);
+    return true;
+  } else {
+    _updateItemChannelLink(itemName, channelUID, conf);
+    return false;
+  }
+};
+
+/**
+ * Removes an ItemChannelLink from the provider. Therefore, the channel link is removed from the Item.
+ *
+ * @memberof items.itemchannellink
+ * @param {string} itemName the name of the Item
+ * @param {string} channelUID
+ * @returns {{itemName: string, configuration: *, channelUID: string}|null} the removed ItemChannelLink or `null` if none exists
+ */
+const removeItemChannelLink = function (itemName, channelUID) {
+  log.debug(`Removing ItemChannelLink ${itemName} -> ${channelUID} from provider...`);
+  const itemChannelLink = managedItemChannelLinkProvider.remove(itemName + ' -> ' + channelUID);
+  if (itemChannelLink === null) return null;
+  return {
+    itemName: itemChannelLink.itemName.toString(),
+    channelUID: itemChannelLink.channelUID.toString(),
+    configuration: itemChannelLink.configuration
+  };
+};
+
+module.exports = {
+  getItemChannelLink,
+  replaceItemChannelLink,
+  removeItemChannelLink
+};
