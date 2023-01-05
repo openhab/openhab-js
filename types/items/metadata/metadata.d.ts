1180a8e5a91b19a464cc82d1d56b965a4c9a98ce
Stefan Höhn <mail@stefanhoehn.com>
2023-01-05 14:53:44 +0100

    [items] metadata: return conf as js obj
    Signed-off-by: Stefan Höhn <mail@stefanhoehn.com>

--- types/items/metadata/metadata.d.ts
+++ types/items/metadata/metadata.d.ts
@@ -4,10 +4,10 @@
  * @memberOf items.metadata
  * @param {string} itemName the name of the Item
  * @param {string} namespace name of the metadata
- * @returns {{configuration: *, value: string}|null} metadata or `null` if the Item has no metadata with the given name
+ * @returns {{configuration: object, value: string}|null} metadata or `null` if the Item has no metadata with the given name
  */
 export function getMetadata(itemName: string, namespace: string): {
-    configuration: any;
+    configuration: object;
     value: string;
 } | null;
 /**
@@ -18,10 +18,10 @@ export function getMetadata(itemName: string, namespace: string): {
  * @param {string} namespace name of the metadata
  * @param {string} value value for this metadata
  * @param {object} [configuration] optional metadata configuration
- * @returns {{configuration: *, value: string}|null} old metadata or `null` if the Item has no metadata with the given name
+ * @returns {{configuration: object, value: string}|null} old metadata or `null` if the Item has no metadata with the given name
  */
 export function replaceMetadata(itemName: string, namespace: string, value: string, configuration?: object): {
-    configuration: any;
+    configuration: object;
     value: string;
 } | null;
 /**
@@ -30,10 +30,10 @@ export function replaceMetadata(itemName: string, namespace: string, value: stri
  * @memberof items.metadata
  * @param {string} itemName the name of the Item
  * @param {string} namespace name of the metadata
- * @returns {{configuration: *, value: string}|null} removed metadata or `null` if the Item has no metadata with the given name
+ * @returns {{configuration: object, value: string}|null} removed metadata or `null` if the Item has no metadata with the given name
  */
 export function removeMetadata(itemName: string, namespace: string): {
-    configuration: any;
+    configuration: object;
     value: string;
 } | null;
 export declare const itemchannellink: typeof import("./itemchannellink");
