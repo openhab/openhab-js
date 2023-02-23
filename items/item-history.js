openhab/openhab-js

commit a42709189cd0be86c24fb80e541c85c6abfee0fe

[items] ItemHistory: Return HistoricItem where applicable (#228)
Fixes #226.

For minimumSince, minimumBetween, maximumSince and maximumBetween the
caller might care about the timestamp of the returned state, so return
an object which contains both state and timestamp.
Additionally, remove previousStateTimestamp and make previousState
return an HistoricItem instead for API symmetry.

Signed-off-by: Danny Baumann <dannybaumann@web.de>

items/item-history.js
@@ -3,6 +3,14 @@ const time = require('../time');
 const PersistenceExtensions = Java.type('org.openhab.core.persistence.extensions.PersistenceExtensions');
 
 /**
+ * @typedef {object} HistoricItem
+ * @property {string} state Item state
+ * @property {HostState} rawState Raw Java state
+ * @property {number|null} numericState Numeric representation of Item state, or null if state is not numeric
+ * @property {time.ZonedDateTime} timestamp timestamp of historic item
+ */
+
+/**
  * Class representing the historic state of an openHAB Item.
  * If the Item receives its state from a binding that supports units of measurement, the returned state is in the according base unit, otherwise there is no unit conversion happening.
  * Wrapping the {@link https://www.openhab.org/javadoc/latest/org/openhab/core/persistence/extensions/persistenceextensions PersistenceExtensions}.
@@ -201,11 +209,11 @@ class ItemHistory {
    *
    * @param {(time.ZonedDateTime | Date)} timestamp
    * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-   * @returns {(string | null)} state
+   * @returns {(HistoricItem | null)} historic item
    */
   historicState (timestamp, serviceId) {
     try {
-      return this._stateOrNull(PersistenceExtensions.historicState(this.rawItem, ...arguments));
+      return this._historicItemOrNull(PersistenceExtensions.historicState(this.rawItem, ...arguments));
     } catch (e) {
 
     }
@@ -237,12 +245,10 @@ class ItemHistory {
    * @param {(time.ZonedDateTime | Date)} begin begin
    * @param {(time.ZonedDateTime | Date)} end end
    * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-   * @returns {(number | null)} state or null
+   * @returns {(HistoricItem | null)} historic item or null
    */
   maximumBetween (begin, end, serviceId) {
-    const state = this._stateOrNull(PersistenceExtensions.maximumBetween(this.rawItem, ...arguments));
-    if (state === null) return null;
-    return parseFloat(state);
+    return this._historicItemOrNull(PersistenceExtensions.maximumBetween(this.rawItem, ...arguments));
   }
 
   /**
@@ -250,12 +256,10 @@ class ItemHistory {
    *
    * @param {(time.ZonedDateTime | Date)} timestamp
    * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-   * @returns {(number | null)} state or null
+   * @returns {(HistoricItem | null)} historic item or null
    */
   maximumSince (timestamp, serviceId) {
-    const state = this._stateOrNull(PersistenceExtensions.maximumSince(this.rawItem, ...arguments));
-    if (state === null) return null;
-    return parseFloat(state);
+    return this._historicItemOrNull(PersistenceExtensions.maximumSince(this.rawItem, ...arguments));
   }
 
   /**
@@ -264,12 +268,10 @@ class ItemHistory {
    * @param {(time.ZonedDateTime | Date)} begin begin
    * @param {(time.ZonedDateTime | Date)} end end
    * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-   * @returns {(number | null)} state or null
+   * @returns {(HistoricItem | null)} historic item or null
    */
   minimumBetween (begin, end, serviceId) {
-    const state = this._stateOrNull(PersistenceExtensions.minimumBetween(this.rawItem, ...arguments));
-    if (state === null) return null;
-    return parseFloat(state);
+    return this._historicItemOrNull(PersistenceExtensions.minimumBetween(this.rawItem, ...arguments));
   }
 
   /**
@@ -277,12 +279,10 @@ class ItemHistory {
    *
    * @param {(time.ZonedDateTime | Date)} timestamp
    * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-   * @returns {(number | null)} state or null
+   * @returns {(HistoricItem | null)} historic item or null
    */
   minimumSince (timestamp, serviceId) {
-    const state = this._stateOrNull(PersistenceExtensions.minimumSince(this.rawItem, ...arguments));
-    if (state === null) return null;
-    return parseFloat(state);
+    return this._historicItemOrNull(PersistenceExtensions.minimumSince(this.rawItem, ...arguments));
   }
 
   /**
@@ -299,21 +299,10 @@ class ItemHistory {
    *
    * @param {boolean} [skipEqual] optional, if true, skips equal state values and searches the first state not equal the current state
    * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-   * @returns {(string | null)} state or null
+   * @returns {(HistoricItem | null)} historic item or null
    */
   previousState (skipEqual, serviceId) {
-    return this._stateOrNull(PersistenceExtensions.previousState(this.rawItem, ...arguments));
-  }
-
-  /**
-   * Returns the time when the previous state of a given Item was persisted.
-   *
-   * @param {boolean} [skipEqual] optional, if true, skips equal state values and searches the first state not equal the current state
-   * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-   * @returns {(time.ZonedDateTime | null)} {@link time.ZonedDateTime} or null
-   */
-  previousStateTimestamp (skipEqual, serviceId) {
-    return this._timestampOrNull(PersistenceExtensions.previousState(this.rawItem, ...arguments));
+    return this._historicItemOrNull(PersistenceExtensions.previousState(this.rawItem, ...arguments));
   }
 
   /**
@@ -388,20 +377,6 @@ class ItemHistory {
   /**
    * @private
    */
-  _stateOrNull (result) {
-    return result === null ? null : result.getState().toString();
-  }
-
-  /**
-   * @private
-   */
-  _timestampOrNull (result) {
-    return result === null ? null : time.ZonedDateTime.parse(result.getTimestamp().toString());
-  }
-
-  /**
-   * @private
-   */
   _dateOrNull (result) {
     return result === null ? null : time.ZonedDateTime.parse(result.toString());
   }
@@ -411,6 +386,23 @@ class ItemHistory {
    */
   _decimalOrNull (result) {
     return result === null ? null : result.toBigDecimal();
+  }
+
+  /**
+   * @private
+   */
+  _historicItemOrNull (result) {
+    if (result === null) {
+      return null;
+    }
+    const rawState = result.getState();
+    const numericState = parseFloat(rawState.toString());
+    return {
+      state: rawState.toString(),
+      rawState: rawState,
+      numericState: isNaN(numericState) ? null : numericState,
+      timestamp: time.ZonedDateTime.parse(result.getTimestamp().toString())
+    };
   }
 }
 

types/items/item-history.d.ts
@@ -1,5 +1,12 @@
 export = ItemHistory;
 /**
+ * @typedef {object} HistoricItem
+ * @property {string} state Item state
+ * @property {HostState} rawState Raw Java state
+ * @property {number|null} numericState Numeric representation of Item state, or null if state is not numeric
+ * @property {time.ZonedDateTime} timestamp timestamp of historic item
+ */
+/**
  * Class representing the historic state of an openHAB Item.
  * If the Item receives its state from a binding that supports units of measurement, the returned state is in the according base unit, otherwise there is no unit conversion happening.
  * Wrapping the {@link https://www.openhab.org/javadoc/latest/org/openhab/core/persistence/extensions/persistenceextensions PersistenceExtensions}.
@@ -150,9 +157,9 @@ declare class ItemHistory {
      *
      * @param {(time.ZonedDateTime | Date)} timestamp
      * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(string | null)} state
+     * @returns {(HistoricItem | null)} historic item
      */
-    historicState(timestamp: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (string | null);
+    historicState(timestamp: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (HistoricItem | null);
     /**
      * Query the last update time of a given Item.
      *
@@ -173,34 +180,34 @@ declare class ItemHistory {
      * @param {(time.ZonedDateTime | Date)} begin begin
      * @param {(time.ZonedDateTime | Date)} end end
      * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(number | null)} state or null
+     * @returns {(HistoricItem | null)} historic item or null
      */
-    maximumBetween(begin: (time.ZonedDateTime | Date), end: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (number | null);
+    maximumBetween(begin: (time.ZonedDateTime | Date), end: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (HistoricItem | null);
     /**
      * Gets the state with the maximum value of a given Item since a certain point in time.
      *
      * @param {(time.ZonedDateTime | Date)} timestamp
      * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(number | null)} state or null
+     * @returns {(HistoricItem | null)} historic item or null
      */
-    maximumSince(timestamp: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (number | null);
+    maximumSince(timestamp: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (HistoricItem | null);
     /**
      * Gets the state with the minimum value of a given Item between two certain points in time.
      *
      * @param {(time.ZonedDateTime | Date)} begin begin
      * @param {(time.ZonedDateTime | Date)} end end
      * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(number | null)} state or null
+     * @returns {(HistoricItem | null)} historic item or null
      */
-    minimumBetween(begin: (time.ZonedDateTime | Date), end: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (number | null);
+    minimumBetween(begin: (time.ZonedDateTime | Date), end: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (HistoricItem | null);
     /**
      * Gets the state with the minimum value of a given Item since a certain point in time.
      *
      * @param {(time.ZonedDateTime | Date)} timestamp
      * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(number | null)} state or null
+     * @returns {(HistoricItem | null)} historic item or null
      */
-    minimumSince(timestamp: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (number | null);
+    minimumSince(timestamp: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (HistoricItem | null);
     /**
      * Persists the state of a given Item.
      *
@@ -212,17 +219,9 @@ declare class ItemHistory {
      *
      * @param {boolean} [skipEqual] optional, if true, skips equal state values and searches the first state not equal the current state
      * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(string | null)} state or null
+     * @returns {(HistoricItem | null)} historic item or null
      */
-    previousState(skipEqual?: boolean, serviceId?: string, ...args: any[]): (string | null);
-    /**
-     * Returns the time when the previous state of a given Item was persisted.
-     *
-     * @param {boolean} [skipEqual] optional, if true, skips equal state values and searches the first state not equal the current state
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(time.ZonedDateTime | null)} {@link time.ZonedDateTime} or null
-     */
-    previousStateTimestamp(skipEqual?: boolean, serviceId?: string, ...args: any[]): (time.ZonedDateTime | null);
+    previousState(skipEqual?: boolean, serviceId?: string, ...args: any[]): (HistoricItem | null);
     /**
      * Gets the sum of the states of a given Item between two certain points in time.
      *
@@ -277,22 +276,39 @@ declare class ItemHistory {
     /**
      * @private
      */
-    private _stateOrNull;
-    /**
-     * @private
-     */
-    private _timestampOrNull;
-    /**
-     * @private
-     */
     private _dateOrNull;
     /**
      * @private
      */
     private _decimalOrNull;
+    /**
+     * @private
+     */
+    private _historicItemOrNull;
+}
+declare namespace ItemHistory {
+    export { HistoricItem };
 }
 declare namespace time {
     type ZonedDateTime = import('@js-joda/core').ZonedDateTime;
 }
 import time = require("../time");
+type HistoricItem = {
+    /**
+     * Item state
+     */
+    state: string;
+    /**
+     * Raw Java state
+     */
+    rawState: HostState;
+    /**
+     * Numeric representation of Item state, or null if state is not numeric
+     */
+    numericState: number | null;
+    /**
+     * timestamp of historic item
+     */
+    timestamp: time.ZonedDateTime;
+};
 //# sourceMappingURL=item-history.d.ts.map

types/items/item-history.d.ts.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"item-history.d.ts","sourceRoot":"","sources":["../../items/item-history.js"],"names":[],"mappings":";AAIA;;;;;;;;;GASG;AACH;IACE,0BAEC;IADC,aAAsB;IAGxB;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;;;;;OAWG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAInB;IAED;;;;;;OAMG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAInB;IAED;;;;;;;OAOG;IACH,oBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;OAMG;IACH,sBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;;OAOG;IACH,gCALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;OAMG;IACH,kCAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;;OAOG;IACH,oBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,sBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;OAOG;IACH,wBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,0BAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;OAOG;IACH,yBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAK3B;IAED;;;;;;;OAOG;IACH,4BALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,8BAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,yBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAQ3B;IAED;;;;;OAKG;IACH,uBAHW,MAAM,mBACJ,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,CAIvC;IAED;;;;;OAKG;IACH,wBAHW,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAM3B;IAED;;;;;;OAMG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAM3B;IAED;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAM3B;IAED;;;;;;OAMG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAM3B;IAED;;;;OAIG;IACH,oBAFW,MAAM,wBAIhB;IAED;;;;;;OAMG;IACH,0BAJW,OAAO,cACP,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,mCAJW,OAAO,cACP,MAAM,mBACJ,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,CAIvC;IAED;;;;;;;OAOG;IACH,kBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,oBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAInB;IAED;;;;;;OAMG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAInB;IAED;;;;;;;OAOG;IACH,uBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,yBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;OAEG;IACH,qBAEC;IAED;;OAEG;IACH,yBAEC;IAED;;OAEG;IACH,oBAEC;IAED;;OAEG;IACH,uBAEC;CACF;;yBA9Za,OAAO,eAAe,EAAE,aAAa"}
+{"version":3,"file":"item-history.d.ts","sourceRoot":"","sources":["../../items/item-history.js"],"names":[],"mappings":";AAIA;;;;;;GAMG;AAEH;;;;;;;;;GASG;AACH;IACE,0BAEC;IADC,aAAsB;IAGxB;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;;;;;OAWG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAInB;IAED;;;;;;OAMG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAInB;IAED;;;;;;;OAOG;IACH,oBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;OAMG;IACH,sBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;;OAOG;IACH,gCALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;OAMG;IACH,kCAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;;OAOG;IACH,oBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,sBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;OAOG;IACH,wBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,0BAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;OAOG;IACH,yBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAK3B;IAED;;;;;;;OAOG;IACH,4BALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,8BAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,yBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,YAAY,GAAG,IAAI,CAAC,CAQjC;IAED;;;;;OAKG;IACH,uBAHW,MAAM,mBACJ,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,CAIvC;IAED;;;;;OAKG;IACH,wBAHW,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,YAAY,GAAG,IAAI,CAAC,CAIjC;IAED;;;;;;OAMG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,YAAY,GAAG,IAAI,CAAC,CAIjC;IAED;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,YAAY,GAAG,IAAI,CAAC,CAIjC;IAED;;;;;;OAMG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,YAAY,GAAG,IAAI,CAAC,CAIjC;IAED;;;;OAIG;IACH,oBAFW,MAAM,wBAIhB;IAED;;;;;;OAMG;IACH,0BAJW,OAAO,cACP,MAAM,mBACJ,CAAC,YAAY,GAAG,IAAI,CAAC,CAIjC;IAED;;;;;;;OAOG;IACH,kBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,oBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAInB;IAED;;;;;;OAMG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAInB;IAED;;;;;;;OAOG;IACH,uBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,yBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;OAEG;IACH,oBAEC;IAED;;OAEG;IACH,uBAEC;IAED;;OAEG;IACH,4BAYC;CACF;;;;;yBAtZa,OAAO,eAAe,EAAE,aAAa;;;;;;;WAMrC,MAAM;;;;;;;;kBAEN,MAAM,GAAC,IAAI;;;;eACX,KAAK,aAAa"}

commit ddc753d7c395d1f213f48b0d5a1ddffe54cc8329

[items] Fix `ItemHistory.latestState` doesn’t return string (#249)
`rules.SwitchableJSRule` failed due to `ItemHistory.latestState`
returning a `HistoricItem` instead of the expected `string` state.
Regression from #228.

Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

items/item-history.js
@@ -233,13 +233,14 @@ class ItemHistory {
   }
 
   /**
-   * Retrieves the historic item state for a given Item at the current point in time.
+   * Retrieves the historic Item state for a given Item at the current point in time.
    *
    * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
    * @returns {(string | null)} state
    */
   latestState (serviceId) {
-    return this.historicState(time.ZonedDateTime.now(), ...arguments);
+    const result = this.historicState(time.ZonedDateTime.now(), ...arguments);
+    return (result === null) ? null : result.state;
   }
 
   /**

types/items/item-history.d.ts
@@ -169,7 +169,7 @@ declare class ItemHistory {
      */
     lastUpdate(serviceId?: string, ...args: any[]): (time.ZonedDateTime | null);
     /**
-     * Retrieves the historic item state for a given Item at the current point in time.
+     * Retrieves the historic Item state for a given Item at the current point in time.
      *
      * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
      * @returns {(string | null)} state

types/items/item-history.d.ts.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"item-history.d.ts","sourceRoot":"","sources":["../../items/item-history.js"],"names":[],"mappings":";AAUA;;;;;;;GAOG;AAEH;;;;;;;;;GASG;AACH;IACE,0BAEC;IADC,aAAsB;IAGxB;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;;;;;OAWG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAInB;IAED;;;;;;OAMG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAInB;IAED;;;;;;;OAOG;IACH,oBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;OAMG;IACH,sBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;;OAOG;IACH,gCALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;OAMG;IACH,kCAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;;OAOG;IACH,oBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,sBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;OAOG;IACH,wBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,0BAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;OAOG;IACH,yBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAK3B;IAED;;;;;;;OAOG;IACH,4BALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,8BAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,yBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,YAAY,GAAG,IAAI,CAAC,CAIjC;IAED;;;;;OAKG;IACH,uBAHW,MAAM,mBACJ,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,CAIvC;IAED;;;;;OAKG;IACH,wBAHW,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,YAAY,GAAG,IAAI,CAAC,CAIjC;IAED;;;;;;OAMG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,YAAY,GAAG,IAAI,CAAC,CAIjC;IAED;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,YAAY,GAAG,IAAI,CAAC,CAIjC;IAED;;;;;;OAMG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,YAAY,GAAG,IAAI,CAAC,CAIjC;IAED;;;;OAIG;IACH,oBAFW,MAAM,wBAIhB;IAED;;;;;;OAMG;IACH,0BAJW,OAAO,cACP,MAAM,mBACJ,CAAC,YAAY,GAAG,IAAI,CAAC,CAIjC;IAED;;;;;;;OAOG;IACH,kBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,oBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAInB;IAED;;;;;;OAMG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAInB;IAED;;;;;;;OAOG;IACH,uBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,yBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;OAEG;IACH,oBAEC;IAED;;OAEG;IACH,uBAEC;IAED;;OAEG;IACH,4BAuBC;CACF;;;;;yBApaa,OAAO,eAAe,EAAE,aAAa;;;;;;;WAYrC,MAAM;;;;;;;;kBAEN,MAAM,GAAC,IAAI;;;;mBACX,sCAAS,IAAI;;;;eACb,KAAK,aAAa"}
+{"version":3,"file":"item-history.d.ts","sourceRoot":"","sources":["../../items/item-history.js"],"names":[],"mappings":";AAUA;;;;;;;GAOG;AAEH;;;;;;;;;GASG;AACH;IACE,0BAEC;IADC,aAAsB;IAGxB;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;;;;;OAWG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAInB;IAED;;;;;;OAMG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAInB;IAED;;;;;;;OAOG;IACH,oBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;OAMG;IACH,sBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;;OAOG;IACH,gCALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;OAMG;IACH,kCAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;;OAOG;IACH,oBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,sBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;OAOG;IACH,wBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,0BAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;OAOG;IACH,yBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAK3B;IAED;;;;;;;OAOG;IACH,4BALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,8BAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,yBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,YAAY,GAAG,IAAI,CAAC,CAIjC;IAED;;;;;OAKG;IACH,uBAHW,MAAM,mBACJ,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,CAIvC;IAED;;;;;OAKG;IACH,wBAHW,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAK3B;IAED;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,YAAY,GAAG,IAAI,CAAC,CAIjC;IAED;;;;;;OAMG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,YAAY,GAAG,IAAI,CAAC,CAIjC;IAED;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,YAAY,GAAG,IAAI,CAAC,CAIjC;IAED;;;;;;OAMG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,YAAY,GAAG,IAAI,CAAC,CAIjC;IAED;;;;OAIG;IACH,oBAFW,MAAM,wBAIhB;IAED;;;;;;OAMG;IACH,0BAJW,OAAO,cACP,MAAM,mBACJ,CAAC,YAAY,GAAG,IAAI,CAAC,CAIjC;IAED;;;;;;;OAOG;IACH,kBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,oBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAInB;IAED;;;;;;OAMG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAInB;IAED;;;;;;;OAOG;IACH,uBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,yBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;OAEG;IACH,oBAEC;IAED;;OAEG;IACH,uBAEC;IAED;;OAEG;IACH,4BAuBC;CACF;;;;;yBAraa,OAAO,eAAe,EAAE,aAAa;;;;;;;WAYrC,MAAM;;;;;;;;kBAEN,MAAM,GAAC,IAAI;;;;mBACX,sCAAS,IAAI;;;;eACb,KAAK,aAAa"}