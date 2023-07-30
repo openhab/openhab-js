openhab/openhab-js

commit 4e07a60289bbca65a99172a6acfa78b5c118febf

Infrastructure: Introduce `build` folder & Adjust webpack settings (#273)
- Move JSDoc, TypeScript and Webpack (config) files to the `build`
folder.
- Move Jest config to the `test` folder.
- Use Terser plugin for Webpack to keep classnames and function names
(eases debugging and fixes #272).
- CONTRIBUTING.md: Mention Jest unit test & remove mocha.
- DEPLOY.md: Simplify unimportant sections and update npm version
command.
- Bump Jest and Webpack to latest versions.

Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

build/@globals-webpack.config.js
@@ -1,12 +1,24 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  entry: './index.js',
+  entry: './build/@openhab-globals.js',
   mode: 'production',
   performance: {
     hints: false,
     maxEntrypointSize: 512000,
     maxAssetSize: 512000
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          keep_classnames: true,
+          keep_fnames: true
+        }
+      })
+    ]
   },
   externals: [
     {
@@ -49,10 +61,10 @@ module.exports = {
     }
   ],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'openhab.js',
+    path: path.resolve(__dirname, '../dist'),
+    filename: '@openhab-globals.js',
     library: {
-      name: 'openhab',
+      name: '@openhab-globals',
       type: 'umd'
     },
     globalObject: 'this'

.gitignore
@@ -6,7 +6,7 @@ docs
 node_modules
 openhab-*.tgz
 out
-coverage
+test/coverage
 types/@globals-webpack.config.*
 types/@openhab-globals.*
 types/helpers.*

build/@openhab-globals.js
@@ -2,7 +2,7 @@
 
 (function (global) {
   'use strict';
-  Object.assign(globalThis, require('./index'));
+  Object.assign(globalThis, require('../index'));
   // Support legacy NodeJS libraries
   globalThis.global = globalThis;
 })(this);

build/webpack.config.js
@@ -1,12 +1,24 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  entry: './@openhab-globals.js',
+  entry: './index.js',
   mode: 'production',
   performance: {
     hints: false,
     maxEntrypointSize: 512000,
     maxAssetSize: 512000
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          keep_classnames: true,
+          keep_fnames: true
+        }
+      })
+    ]
   },
   externals: [
     {
@@ -49,10 +61,10 @@ module.exports = {
     }
   ],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '@openhab-globals.js',
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'openhab.js',
     library: {
-      name: '@openhab-globals',
+      name: 'openhab',
       type: 'umd'
     },
     globalObject: 'this'

test/javax-measure.mock.js
@@ -1,7 +1,6 @@
 // javax.measure.Unit (https://unitsofmeasurement.github.io/unit-api/site/apidocs/javax/measure/Unit.html)
 class Unit {
   getName = jest.fn()
-  getSymbol = jest.fn()
 }
 
 module.exports = { Unit };

test/jest.config.js
@@ -124,13 +124,13 @@ module.exports = {
   // rootDir: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
-  roots: ['test'],
+  roots: ['.'],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  setupFiles: ['./test/java.mock.js', './test/@runtime.mock.js'],
+  setupFiles: ['./java.mock.js', './@runtime.mock.js'],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   // setupFilesAfterEnv: [],

commit 9188abc4f32e0ed42b68223b3816aad36cbb9225

Infrastructure: Disable Webpack TerserPlugin for cached globals injection (#283)
Regression from #273.

Using the Webpack TerserPlugin to minimize web-packed, cached globals injection heavily decreased performance, therefore remove it.

Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

build/@globals-webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './build/@openhab-globals.js',
@@ -9,17 +8,7 @@ module.exports = {
     maxEntrypointSize: 512000,
     maxAssetSize: 512000
   },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          keep_classnames: true,
-          keep_fnames: true
-        }
-      })
-    ]
-  },
+  // Do NOT set up "minimize" using "TerserPlugin", as this (very) negatively affects performance for the cached version
   externals: [
     {
       '@runtime': {