5ad1938f4cb6c8f54a507b5bd2fe1a20c5c27dba
Florian Hotze <florianh_dev@icloud.com>
2023-11-18 16:46:44 +0100

    [rules] Fix rule builder type definitions not available
    Due to a wrong path definition in the main TypeScript definition file, rule builder type defs were not loaded.
    
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

--- types/openhab-js.d.ts
+++ types/openhab-js.d.ts
@@ -58,7 +58,7 @@ declare global {
   type QuantityType = object;
 }
 export const log: typeof import("./log");
-export const rules: typeof import("./rules/rules");
+export const rules: typeof import("./rules");
 export const items: typeof import("./items/items");
 export const things: typeof import("./things");
 export const triggers: typeof import("./triggers");
