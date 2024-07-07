openhab/openhab-js

commit cd3fc4ea5c2b846350d2c087502f438cd2955cd7

[items] ItemHistory: Adjust to breaking changes & Add new methods for future states (#331)
Refs https://github.com/openhab/openhab-core/pull/3736.

- Rename `Item::history` to `Item::persistence`.
- Rename `HistoricItem` to `PersistedItem` and introduce `PersistedState` for state as string, Quantity & number.
- Make variance, deviation, average, sum & delta return the state as `PersistedState` instead of number.
- Add `**Until` methods for all existing queries.
- Add `nextState` and `nextUpdate` methods.
- Rename `historicState` method to `persistedState`.
- Remove `latestState` method.
- Remove deprecated `evolutionRate` method.
- Add `removeAllStatesSince`, `removeAllStatesUntil` & `removeAllStatesBetween` methods.

---------

Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

src/rules/rules.js
@@ -320,7 +320,7 @@ function SwitchableJSRule (ruleConfig) {
     // possibly load item's prior state
     let historicState = null;
     try {
-      historicState = item.history.latestState();
+      historicState = item.persistence.latestState();
     } catch (e) {
       log.warn(`Failed to get historic state of ${item.name} for rule ${ruleConfig.name}: ${e}`);
     }

src/items/items.js
@@ -8,7 +8,7 @@ const osgi = require('../osgi');
 const utils = require('../utils');
 const log = require('../log')('items');
 const metadata = require('./metadata/metadata');
-const { ItemHistory } = require('./item-history');
+const { ItemPersistence } = require('./item-persistence');
 const ItemSemantics = require('./item-semantics');
 const { getQuantity, QuantityError } = require('../quantity');
 
@@ -96,10 +96,10 @@ class Item {
     this.rawItem = rawItem;
 
     /**
-     * Access historical states for this Item {@link items.ItemHistory}
-     * @type {ItemHistory}
+     * Access historical states for this Item {@link items.ItemPersistence}
+     * @type {ItemPersistence}
      */
-    this.history = new ItemHistory(rawItem);
+    this.persistence = new ItemPersistence(rawItem);
 
     /**
      * Access Semantic information of this Item {@link items.ItemSemantics}

types/items/item-history.d.ts
@@ -1,338 +1,0 @@
-export type Quantity = import('../quantity').Quantity;
-/**
- * Class representing the historic state of an openHAB Item.
- * If the Item receives its state from a binding that supports units of measurement, the returned state is in the according base unit, otherwise there is no unit conversion happening.
- * Wrapping the {@link https://www.openhab.org/javadoc/latest/org/openhab/core/persistence/extensions/persistenceextensions PersistenceExtensions}.
- *
- * Be warned: This class can throw several exceptions from the underlying Java layer. It is recommended to wrap the methods of this class inside a try_catch block!
- *
- * @memberOf items
- * @hideconstructor
- */
-export class ItemHistory {
-    constructor(rawItem: any);
-    rawItem: any;
-    /**
-     * Gets the average value of the state of a given Item between two certain points in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} begin begin
-     * @param {(time.ZonedDateTime | Date)} end end
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(number | null)}
-     */
-    averageBetween(begin: (time.ZonedDateTime | Date), end: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (number | null);
-    /**
-     * Gets the average value of the state of a given Item since a certain point in time.
-     *
-     * @example
-     * var yesterday = new Date(new Date().getTime() - (24 * 60 * 60 * 1000));
-     * var item = items.getItem('KitchenDimmer');
-     * console.log('KitchenDimmer average since yesterday', item.history.averageSince(yesterday));
-     *
-     * @param {(time.ZonedDateTime | Date)} timestamp
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(number | null)}
-     */
-    averageSince(timestamp: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (number | null);
-    /**
-     * Checks if the state of a given Item has changed between two certain points in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} begin begin
-     * @param {(time.ZonedDateTime | Date)} end end
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {boolean}
-     */
-    changedBetween(begin: (time.ZonedDateTime | Date), end: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): boolean;
-    /**
-     * Checks if the state of a given Item has changed since a certain point in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} timestamp
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {boolean}
-     */
-    changedSince(timestamp: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): boolean;
-    /**
-     * Gets the number of available historic data points of a given Item between two certain points in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} begin begin
-     * @param {(time.ZonedDateTime | Date)} end end
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {number}
-     */
-    countBetween(begin: (time.ZonedDateTime | Date), end: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): number;
-    /**
-     * Gets the number of available historic data points of a given Item since a certain point in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} timestamp
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {number}
-     */
-    countSince(timestamp: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): number;
-    /**
-     * Gets the number of changes in historic data points of a given Item between two certain points in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} begin begin
-     * @param {(time.ZonedDateTime | Date)} end end
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {number}
-     */
-    countStateChangesBetween(begin: (time.ZonedDateTime | Date), end: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): number;
-    /**
-     * Gets the number of changes in historic data points of a given Item since a certain point in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} timestamp
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {number}
-     */
-    countStateChangesSince(timestamp: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): number;
-    /**
-     * Gets the difference value of the state of a given Item between two certain points in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} begin begin
-     * @param {(time.ZonedDateTime | Date)} end end
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(number | null)}
-     */
-    deltaBetween(begin: (time.ZonedDateTime | Date), end: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (number | null);
-    /**
-     * Gets the difference value of the state of a given Item since a certain point in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} timestamp
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(number | null)}
-     */
-    deltaSince(timestamp: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (number | null);
-    /**
-     * Gets the standard deviation of the state of the given Item between two certain points in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} begin begin
-     * @param {(time.ZonedDateTime | Date)} end end
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(number | null)}
-     */
-    deviationBetween(begin: (time.ZonedDateTime | Date), end: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (number | null);
-    /**
-     * Gets the standard deviation of the state of the given Item since a certain point in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} timestamp
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(number | null)}
-     */
-    deviationSince(timestamp: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (number | null);
-    /**
-     * Gets the evolution rate of the state of a given Item since a certain point in time.
-     *
-     * @deprecated Replaced by evolutionRateSince and evolutionRateBetween.
-     * @param {(time.ZonedDateTime | Date)} timestamp
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(number | null)}
-     */
-    evolutionRate(timestamp: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (number | null);
-    /**
-     * Gets the evolution rate of the state of a given Item between two certain points in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} begin begin
-     * @param {(time.ZonedDateTime | Date)} end end
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(number | null)}
-     */
-    evolutionRateBetween(begin: (time.ZonedDateTime | Date), end: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (number | null);
-    /**
-     * Gets the evolution rate of the state of a given Item since a certain point in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} timestamp
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(number | null)}
-     */
-    evolutionRateSince(timestamp: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (number | null);
-    /**
-     * Retrieves the {@link HistoricItems} for for a given Item between two certain points in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} begin begin
-     * @param {(time.ZonedDateTime | Date)} end end
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {HistoricItem[]}
-     */
-    getAllStatesBetween(begin: (time.ZonedDateTime | Date), end: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): HistoricItem[];
-    /**
-     * Retrieves the {@link HistoricItems} for for a given Item since a certain point in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} timestamp
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {HistoricItem[]}
-     */
-    getAllStatesSince(timestamp: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): HistoricItem[];
-    /**
-     * Retrieves the historic state for a given Item at a certain point in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} timestamp
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(HistoricItem | null)} historic item
-     */
-    historicState(timestamp: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (HistoricItem | null);
-    /**
-     * Query the last update time of a given Item.
-     *
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(time.ZonedDateTime | null)}
-     */
-    lastUpdate(serviceId?: string, ...args: any[]): (time.ZonedDateTime | null);
-    /**
-     * Retrieves the historic Item state for a given Item at the current point in time.
-     *
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(string | null)} state
-     */
-    latestState(serviceId?: string, ...args: any[]): (string | null);
-    /**
-     * Gets the state with the maximum value of a given Item between two certain points in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} begin begin
-     * @param {(time.ZonedDateTime | Date)} end end
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(HistoricItem | null)} historic item or null
-     */
-    maximumBetween(begin: (time.ZonedDateTime | Date), end: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (HistoricItem | null);
-    /**
-     * Gets the state with the maximum value of a given Item since a certain point in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} timestamp
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(HistoricItem | null)} historic item or null
-     */
-    maximumSince(timestamp: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (HistoricItem | null);
-    /**
-     * Gets the state with the minimum value of a given Item between two certain points in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} begin begin
-     * @param {(time.ZonedDateTime | Date)} end end
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(HistoricItem | null)} historic item or null
-     */
-    minimumBetween(begin: (time.ZonedDateTime | Date), end: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (HistoricItem | null);
-    /**
-     * Gets the state with the minimum value of a given Item since a certain point in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} timestamp
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(HistoricItem | null)} historic item or null
-     */
-    minimumSince(timestamp: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (HistoricItem | null);
-    /**
-     * Persists the state of a given Item.
-     *
-     * Tells the persistence service to store the current state of the Item, which is then performed asynchronously.
-     * This has the side effect, that if the Item state changes shortly after `.persist` has been called, the new state will be persisted.
-     * To work around that side effect, you might add `java.lang.Thread.sleep` to your code:
-     * @example
-     * items.MyItem.history.persist(); // Tell persistence to store the current Item state
-     * java.lang.Thread.sleep(100); // Wait 100 ms to make sure persistence has enough time to store the current Item state
-     * items.MyItem.postUpdate(0); // Now set the Item state to a new value
-     *
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     */
-    persist(serviceId?: string, ...args: any[]): void;
-    /**
-     * Returns the previous state of a given Item.
-     *
-     * @param {boolean} [skipEqual] optional, if true, skips equal state values and searches the first state not equal the current state
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(HistoricItem | null)} historic item or null
-     */
-    previousState(skipEqual?: boolean, serviceId?: string, ...args: any[]): (HistoricItem | null);
-    /**
-     * Gets the sum of the states of a given Item between two certain points in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} begin begin
-     * @param {(time.ZonedDateTime | Date)} end end
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(number | null)}
-     */
-    sumBetween(begin: (time.ZonedDateTime | Date), end: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (number | null);
-    /**
-     * Gets the sum of the states of a given Item since a certain point in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} timestamp
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(number | null)}
-     */
-    sumSince(timestamp: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (number | null);
-    /**
-     * Checks if the state of a given Item has been updated between two certain points in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} begin begin
-     * @param {(time.ZonedDateTime | Date)} end end
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {boolean}
-     */
-    updatedBetween(begin: (time.ZonedDateTime | Date), end: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): boolean;
-    /**
-     * Checks if the state of a given Item has been updated since a certain point in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} timestamp
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {boolean}
-     */
-    updatedSince(timestamp: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): boolean;
-    /**
-     * Gets the variance of the state of the given Item between two certain points in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} begin begin
-     * @param {(time.ZonedDateTime | Date)} end end
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(number | null)}
-     */
-    varianceBetween(begin: (time.ZonedDateTime | Date), end: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (number | null);
-    /**
-     * Gets the variance of the state of the given Item since a certain point in time.
-     *
-     * @param {(time.ZonedDateTime | Date)} timestamp
-     * @param {string} [serviceId] Optional persistence service ID, if omitted, the default persistence service will be used.
-     * @returns {(number | null)}
-     */
-    varianceSince(timestamp: (time.ZonedDateTime | Date), serviceId?: string, ...args: any[]): (number | null);
-}
-/**
- * Class representing an openHAB HistoricItem
- *
- * @memberof items
- * @hideconstructor
- */
-export class HistoricItem {
-    /**
-     * @param {*} rawHistoricItem {@link https://www.openhab.org/javadoc/latest/org/openhab/core/persistence/historicitem org.openhab.core.persistence.HistoricItem}
-     */
-    constructor(rawHistoricItem: any);
-    rawHistoricItem: any;
-    /**
-     * Raw Java Item state
-     * @type {HostState}
-     */
-    rawState: HostState;
-    /**
-     * String representation of the Item state.
-     * @type {string}
-     */
-    get state(): string;
-    /**
-     * Numeric representation of Item state, or `null` if state is not numeric
-     * @type {number|null}
-     */
-    get numericState(): number;
-    /**
-     * Item state as {@link Quantity} or `null` if state is not Quantity-compatible or Quantity would be unit-less (without unit)
-     * @type {Quantity|null}
-     */
-    get quantityState(): import("../quantity").Quantity;
-    /**
-     * Timestamp of persisted Item.
-     * @type {time.ZonedDateTime}
-     */
-    get timestamp(): JSJoda.ZonedDateTime;
-}
-declare namespace time {
-    type ZonedDateTime = import('@js-joda/core').ZonedDateTime;
-}
-import time = require("../time");
-export {};
-//# sourceMappingURL=item-history.d.ts.map

types/items/item-history.d.ts.map
@@ -1,1 +1,0 @@
-{"version":3,"file":"item-history.d.ts","sourceRoot":"","sources":["../../src/items/item-history.js"],"names":[],"mappings":"uBAOa,OAAO,aAAa,EAAE,QAAQ;AA4F3C;;;;;;;;;GASG;AACH;IACE,0BAEC;IADC,aAAsB;IAGxB;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;;;;;OAWG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAInB;IAED;;;;;;OAMG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAInB;IAED;;;;;;;OAOG;IACH,oBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;OAMG;IACH,sBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;;OAOG;IACH,gCALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;OAMG;IACH,kCAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;;OAOG;IACH,oBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,sBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;OAOG;IACH,wBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,0BAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;OAOG;IACH,yBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAK3B;IAED;;;;;;;OAOG;IACH,4BALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,8BAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;OAOG;IACH,2BALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,YAAY,EAAE,CAI1B;IAED;;;;;;OAMG;IACH,6BAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,YAAY,EAAE,CAI1B;IAED;;;;;;OAMG;IACH,yBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,YAAY,GAAG,IAAI,CAAC,CAIjC;IAED;;;;;OAKG;IACH,uBAHW,MAAM,mBACJ,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,CAIvC;IAED;;;;;OAKG;IACH,wBAHW,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAK3B;IAED;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,YAAY,GAAG,IAAI,CAAC,CAIjC;IAED;;;;;;OAMG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,YAAY,GAAG,IAAI,CAAC,CAIjC;IAED;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,YAAY,GAAG,IAAI,CAAC,CAIjC;IAED;;;;;;OAMG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,YAAY,GAAG,IAAI,CAAC,CAIjC;IAED;;;;;;;;;;;;OAYG;IACH,oBAFW,MAAM,wBAIhB;IAED;;;;;;OAMG;IACH,0BAJW,OAAO,cACP,MAAM,mBACJ,CAAC,YAAY,GAAG,IAAI,CAAC,CAIjC;IAED;;;;;;;OAOG;IACH,kBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,oBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAInB;IAED;;;;;;OAMG;IACH,wBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAInB;IAED;;;;;;;OAOG;IACH,uBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;IAED;;;;;;OAMG;IACH,yBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAI3B;CACF;AA7dD;;;;;GAKG;AACH;IACE;;OAEG;IACH,kCAOC;IANC,qBAAsC;IACtC;;;OAGG;IACH,oBAA0C;IAG5C;;;OAGG;IACH,oBAEC;IAED;;;OAGG;IACH,2BAGC;IAED;;;OAGG;IACH,oDAWC;IAED;;;OAGG;IACH,sCAEC;CACF;;yBAxEY,OAAO,eAAe,EAAE,aAAa"}

commit 2ea578aefcc7384f100fdcec59a689b67503e533

[rules] SwitchableJSRule: Fix restore previous state fails for switch Item (#355)
Regression from #331.

Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

src/rules/rules.js
@@ -56,13 +56,15 @@
 const GENERATED_RULE_ITEM_TAG = 'GENERATED_RULE_ITEM';
 
 const items = require('../items/items');
-const utils = require('../utils');
+const { randomUUID, jsArrayToJavaSet } = require('../utils');
 const log = require('../log')('rules');
-const osgi = require('../osgi');
+const { getService } = require('../osgi');
 const triggers = require('../triggers');
+const time = require('../time');
+
 const { automationManager, ruleRegistry } = require('@runtime/RuleSupport');
 
-const RuleManager = osgi.getService('org.openhab.core.automation.RuleManager');
+const RuleManager = getService('org.openhab.core.automation.RuleManager');
 
 /**
   * Links an Item to a rule. When the Item is switched on or off, so will the rule be.
@@ -220,7 +222,7 @@ function setEnabled (uid, isEnabled) {
   * @throws {Error} an error if the rule with the passed in uid already exists and {@link RuleConfig.overwrite} is not `true`
   */
 function JSRule (ruleConfig) {
-  const ruleUID = ruleConfig.id || ruleConfig.name.replace(/[^\w]/g, '-') + '-' + utils.randomUUID();
+  const ruleUID = ruleConfig.id || ruleConfig.name.replace(/[^\w]/g, '-') + '-' + randomUUID();
   if (ruleConfig.overwrite === true) {
     removeRule(ruleUID);
   }
@@ -262,7 +264,7 @@ function JSRule (ruleConfig) {
     rule.setName(ruleConfig.name);
   }
   if (ruleConfig.tags) {
-    rule.setTags(utils.jsArrayToJavaSet(ruleConfig.tags));
+    rule.setTags(jsArrayToJavaSet(ruleConfig.tags));
   }
 
   // Register rule here
@@ -321,7 +323,7 @@ function SwitchableJSRule (ruleConfig) {
     // possibly load item's prior state
     let historicState = null;
     try {
-      historicState = item.persistence.latestState();
+      historicState = item.persistence.persistedState(time.ZonedDateTime.now()).state;
     } catch (e) {
       log.warn(`Failed to get historic state of ${item.name} for rule ${ruleConfig.name}: ${e}`);
     }

types/rules/rules.d.ts.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"rules.d.ts","sourceRoot":"","sources":["../../src/rules/rules.js"],"names":[],"mappings":";;;;;;;;;;;cAec,MAAM;;;;cACN,MAAM;;;;mBACN,MAAM;;;;qBACN,MAAM;;;;cACN,MAAM;;;;eACN,MAAM;;;;mBACN,MAAM;;;;gBACN,MAAM;;;;eACN,MAAM;;;;eACN,MAAM;;;;YACN,MAAM;;;;cACN,MAAM;;;;oBACN,MAAM;;;;UACN,MAAM;;;;eACN,MAAM;;;;iBACN,MAAM;;;;gBACN,MAAM;;;;YACN,MAAM;;;;;;;;;;;;;mCAOT,WAAW;;;;;;;;UAKR,MAAM;;;;kBACN,MAAM;;;;cACN,cAAY,aAAa;;;;aACzB,YAAY;;;;SACZ,MAAM;;;;WACN,MAAM,EAAE;;;;gBACR,MAAM;;;;gBACN,OAAO;;;;qBACP,MAAM;;AAgFpB;;;;;;;IAOI;AACJ,gCAHY,MAAM,GACJ,OAAO,CAUpB;AAED;;;;;;;;;IASI;AACJ,6BALY,MAAM,SACN,MAAM,SACN,OAAO,QAalB;AAED;;;;;;;;IAQI;AACJ,+BAJY,MAAM,GACJ,OAAO,CAQpB;AAED;;;;;;;IAOI;AACJ,gCAJY,MAAM,aACN,OAAO,QAQlB;AAED;;;;;;;;;;;;;;;;;IAiBI;AACJ,mCAJY,UAAU,YAiErB;AAED;;;;;;;;;GASG;AACH,6CAJW,UAAU,YA8CpB"}
+{"version":3,"file":"rules.d.ts","sourceRoot":"","sources":["../../src/rules/rules.js"],"names":[],"mappings":";;;;;;;;;;;cAec,MAAM;;;;cACN,MAAM;;;;mBACN,MAAM;;;;qBACN,MAAM;;;;cACN,MAAM;;;;eACN,MAAM;;;;mBACN,MAAM;;;;gBACN,MAAM;;;;eACN,MAAM;;;;eACN,MAAM;;;;YACN,MAAM;;;;cACN,MAAM;;;;oBACN,MAAM;;;;UACN,MAAM;;;;eACN,MAAM;;;;iBACN,MAAM;;;;gBACN,MAAM;;;;YACN,MAAM;;;;;;;;;;;;;mCAOT,WAAW;;;;;;;;UAKR,MAAM;;;;kBACN,MAAM;;;;cACN,cAAY,aAAa;;;;aACzB,YAAY;;;;SACZ,MAAM;;;;WACN,MAAM,EAAE;;;;gBACR,MAAM;;;;gBACN,OAAO;;;;qBACP,MAAM;;AAkFpB;;;;;;;IAOI;AACJ,gCAHY,MAAM,GACJ,OAAO,CAUpB;AAED;;;;;;;;;IASI;AACJ,6BALY,MAAM,SACN,MAAM,SACN,OAAO,QAalB;AAED;;;;;;;;IAQI;AACJ,+BAJY,MAAM,GACJ,OAAO,CAQpB;AAED;;;;;;;IAOI;AACJ,gCAJY,MAAM,aACN,OAAO,QAQlB;AAED;;;;;;;;;;;;;;;;;IAiBI;AACJ,mCAJY,UAAU,YAiErB;AAED;;;;;;;;;GASG;AACH,6CAJW,UAAU,YA8CpB"}