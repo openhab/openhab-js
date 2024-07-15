f9960303a2ac7aa51708eb9559bde0fa4df6362a
Florian Hotze <florianh_dev@icloud.com>
2024-07-15 14:20:41 +0200

    Collect Jest test coverage
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

--- jest.config.js
+++ jest.config.js
@@ -20,10 +20,10 @@ module.exports = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: ['src/**/*.js'],
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: 'coverage',
+  coverageDirectory: 'test/coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -130,7 +130,7 @@ module.exports = {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  setupFiles: ['./jest.setup.js', './@runtime.mock.js'],
+  setupFiles: ['./test/jest.setup.js', './test/@runtime.mock.js'],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   // setupFilesAfterEnv: [],
