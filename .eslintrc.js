27edb5d04660de26d76811a89f4e7de5021d12c9
Florian Hotze <dev@florianhotze.com>
2025-02-05 10:18:38 +0100

    Apply ESLint to .eslintrc.js
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-    extends: 'eslint-config-standard',
-    rules: {
-        semi: ["error", "always"],
-        "no-extra-semi": "error",
-        "func-style": ["error", "declaration", { "allowArrowFunctions": true }]
-    },
-    globals: {
-        Java: "readonly"
-    },
-    parserOptions: {
-        ecmaVersion: 2022
-    }
-}
+  extends: 'eslint-config-standard',
+  rules: {
+    semi: ['error', 'always'],
+    'no-extra-semi': 'error',
+    'func-style': ['error', 'declaration', { allowArrowFunctions: true }]
+  },
+  globals: {
+    Java: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2022
+  }
+};
