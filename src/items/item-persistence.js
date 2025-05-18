openhab/openhab-js

commit 1815a86064781bdbdb8b0b78d99591119cb528db

[items] Refactor state factory methods into shared helpers to reduce code duplication (#437)
Signed-off-by: Florian Hotze <dev@florianhotze.com>

src/items/item-persistence.js
@@ -1,7 +1,7 @@
 const log = require('../log')('items.ItemPersistence');
 const time = require('../time');
-const { getQuantity, QuantityError } = require('../quantity');
 const { _toOpenhabPrimitiveType, _isTimeSeries } = require('../helpers');
+const { _numericStateOrNull, _quantityStateOrNull } = require('./helpers');
 
 const PersistenceExtensions = Java.type('org.openhab.core.persistence.extensions.PersistenceExtensions');
 const TimeSeries = Java.type('org.openhab.core.types.TimeSeries');
@@ -61,8 +61,7 @@ class PersistedState {
    * @type {number|null}
    */
   get numericState () {
-    const numericState = parseFloat(this.rawState.toString());
-    return isNaN(numericState) ? null : numericState;
+    return _numericStateOrNull(this.rawState);
   }
 
   /**
@@ -70,16 +69,7 @@ class PersistedState {
    * @type {Quantity|null}
    */
   get quantityState () {
-    try {
-      const qty = getQuantity(this.rawState.toString());
-      return (qty !== null && qty.symbol !== null) ? qty : null;
-    } catch (e) {
-      if (e instanceof QuantityError) {
-        return null;
-      } else {
-        throw Error('Failed to create "quantityState": ' + e);
-      }
-    }
+    _quantityStateOrNull(this.rawState);
   }
 
   toString () {

.gitignore
@@ -12,6 +12,6 @@ out
 test/coverage
 types/@globals-webpack.config.*
 types/@openhab-globals.*
-types/helpers.*
+types/**/helpers.*
 types/index.*
 types/typeOfArguments.*

src/helpers.js
@@ -1,4 +1,5 @@
 // Helper functions used internally across the library
+// This module MUST NOT depend on any other library code to avoid circular dependencies
 
 const javaZDT = Java.type('java.time.ZonedDateTime');
 const javaDuration = Java.type('java.time.Duration');

src/items/helpers.js
@@ -1,0 +1,61 @@
+// Helper functions used internally across the items namespace
+
+const { PercentType } = require('@runtime');
+
+const { getQuantity, QuantityError } = require('../quantity');
+
+/**
+ * Return a string representation of a state.
+ *
+ * @private
+ * @param {HostState|null} rawState the state
+ * @returns {string|null} string representation or `null` if `rawState` was `null`
+ */
+function _stateOrNull (rawState) {
+  if (rawState === null) return null;
+  return rawState.toString();
+}
+
+/**
+ * Return a numeric representation of a state.
+ *
+ * @private
+ * @param {HostState|null} rawState the state
+ * @param {string} [type] the type of the Item
+ * @returns {number|null} numeric representation or `null` if `rawState` was `null`
+ */
+function _numericStateOrNull (rawState, type) {
+  if (rawState === null) return null;
+  let state = rawState.toString();
+  if (type === 'Color') state = rawState.as(PercentType).toString();
+  const numericState = parseFloat(state);
+  return isNaN(numericState) ? null : numericState;
+}
+
+/**
+ * Return a Quantity representation of a state.
+ *
+ * @private
+ * @param {HostState} rawState the state
+ * @returns {Quantity|null} Quantity representation, or `null` if `rawState` was `null` or not Quantity-compatible, Quantity would be unit-less (without unit) or not available
+ * @throws failed to create quantityState
+ */
+function _quantityStateOrNull (rawState) {
+  if (rawState === null) return null;
+  try {
+    const qty = getQuantity(rawState);
+    return (qty !== null && qty.symbol !== null) ? qty : null;
+  } catch (e) {
+    if (e instanceof QuantityError) {
+      return null;
+    } else {
+      throw Error('Failed to create "quantityState": ' + e);
+    }
+  }
+}
+
+module.exports = {
+  _stateOrNull,
+  _numericStateOrNull,
+  _quantityStateOrNull
+};

src/items/items.js
@@ -9,11 +9,11 @@ const utils = require('../utils');
 const log = require('../log')('items');
 const { _toOpenhabPrimitiveType, _isQuantity, _isItem } = require('../helpers');
 const cache = require('../cache');
-const { getQuantity, QuantityError } = require('../quantity');
 const time = require('../time');
 
-const { OnOffType, PercentType, UnDefType, events, itemRegistry } = require('@runtime');
+const { OnOffType, UnDefType, events, itemRegistry } = require('@runtime');
 
+const { _stateOrNull, _numericStateOrNull, _quantityStateOrNull } = require('./helpers');
 const metadata = require('./metadata/metadata');
 const ItemPersistence = require('./item-persistence');
 const ItemSemantics = require('./item-semantics');
@@ -601,56 +601,6 @@ function _createItem (itemConfig) {
   } catch (e) {
     log.error('Failed to create Item: ' + e);
     throw e;
-  }
-}
-
-/**
- * Return a string representation of a state.
- *
- * @private
- * @param {HostState|null} rawState the state
- * @returns {string|null} string representation or `null` if `rawState` was `null`
- */
-function _stateOrNull (rawState) {
-  if (rawState === null) return null;
-  return rawState.toString();
-}
-
-/**
- * Return a numeric representation of a state.
- *
- * @private
- * @param {HostState|null} rawState the state
- * @param {string} [type] the type of the Item
- * @returns {number|null} numeric representation or `null` if `rawState` was `null`
- */
-function _numericStateOrNull (rawState, type) {
-  if (rawState === null) return null;
-  let state = rawState.toString();
-  if (type === 'Color') state = rawState.as(PercentType).toString();
-  const numericState = parseFloat(state);
-  return isNaN(numericState) ? null : numericState;
-}
-
-/**
- * Return a Quantity representation of a state.
- *
- * @private
- * @param {HostState} rawState the state
- * @returns {Quantity|null} Quantity representation, or `null` if `rawState` was `null` or not Quantity-compatible, Quantity would be unit-less (without unit) or not available
- * @throws failed to create quantityState
- */
-function _quantityStateOrNull (rawState) {
-  if (rawState === null) return null;
-  try {
-    const qty = getQuantity(rawState);
-    return (qty !== null && qty.symbol !== null) ? qty : null;
-  } catch (e) {
-    if (e instanceof QuantityError) {
-      return null;
-    } else {
-      throw Error('Failed to create "quantityState": ' + e);
-    }
   }
 }
 

types/items/item-persistence.d.ts.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"item-persistence.d.ts","sourceRoot":"","sources":["../../src/items/item-persistence.js"],"names":[],"mappings":";AA+JA;;;;;;;;;;;GAWG;AACH;IAKE;;;;OAIG;IACH,8BAKC;IAdD,0BAEC;IADC,aAAsB;IAexB;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;OA+BG;IACH,oBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,UAC3B,MAAM,GAAC,MAAM,GAAC,KAAK,aAAa,GAAC,QAAQ,YAAU,eACnD,gBAAgB,cAChB,MAAM,wBAiChB;IAqDD;;;;;;OAMG;IACH,0BAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,aAAa,GAAG,IAAI,CAAC,CAIlC;IAED;;;;;;MAME;IACF,uBAJW,MAAM,mBACJ,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,CAKvC;IAED;;;;;OAKG;IACH,uBAHW,MAAM,mBACJ,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,CAIvC;IAED;;;;;;OAMG;IACH,uBAJW,MAAM,mBACJ,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,CAKvC;IAED;;;;;OAKG;IACH,uBAHW,MAAM,mBACJ,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,CAIvC;IAED;;;;;;OAMG;IACH,0BAJW,OAAO,cACP,MAAM,mBACJ,CAAC,aAAa,GAAG,IAAI,CAAC,CAIlC;IAED;;;;;;OAMG;IACH,sBAJW,OAAO,cACP,MAAM,mBACJ,CAAC,aAAa,GAAG,IAAI,CAAC,CAIlC;IAED;;;;;;;OAOG;IACH,wBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAKnB;IAED;;;;;;;OAOG;IACH,wBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAKnB;IAED;;;;;;;;OAQG;IACH,sBANW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAKnB;IAED;;;;;;;OAOG;IACH,wBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAKnB;IAED;;;;;;;OAOG;IACH,wBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAKnB;IAED;;;;;;;;OAQG;IACH,sBANW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAKnB;IAED;;;;;;;OAOG;IACH,wBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,aAAa,GAAG,IAAI,CAAC,CAKlC;IAED;;;;;;;OAOG;IACH,wBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,aAAa,GAAG,IAAI,CAAC,CAKlC;IAED;;;;;;;;OAQG;IACH,sBANW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,aAAa,GAAG,IAAI,CAAC,CAKlC;IAED;;;;;;;OAOG;IACH,wBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,aAAa,GAAG,IAAI,CAAC,CAKlC;IAED;;;;;;;OAOG;IACH,wBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,aAAa,GAAG,IAAI,CAAC,CAKlC;IAED;;;;;;;;OAQG;IACH,sBANW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,aAAa,GAAG,IAAI,CAAC,CAKlC;IAED;;;;;;;;;;;OAWG;IACH,yBATW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,iCAI3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAMnC;IAED;;;;;;;;;;;OAWG;IACH,yBATW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,iCAI3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAMnC;IAED;;;;;;;;;;;;OAYG;IACH,uBAVW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,iCAI3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAMnC;IAED;;;;;;;;;;;OAWG;IACH,0BATW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,iCAI3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAMnC;IAED;;;;;;;;;;;OAWG;IACH,0BATW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,iCAI3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAMnC;IAED;;;;;;;;;;;;OAYG;IACH,wBAVW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,iCAI3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAMnC;IAED;;;;;;;;;;;;;;OAcG;IACH,wBAPW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,iCAI3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAInC;IAED;;;;;;;;;OASG;IACH,wBAPW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,iCAI3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAInC;IAED;;;;;;;;;;OAUG;IACH,sBARW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,iCAI3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAInC;IAED;;;;;;;;;;;;;;OAcG;IACH,2BAPW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,iCAI3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAInC;IAED;;;;;;;;;MASE;IACF,2BAPU,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,iCAI3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAIlC;IAED;;;;;;;;;;MAUE;IACF,yBARU,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,iCAI3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAIlC;IAED;;;;;;;;;;;OAWG;IACH,uBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAInC;IAED;;;;;;OAMG;IACH,uBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAInC;IAED;;;;;;;OAOG;IACH,qBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAInC;IAED;;;;;;OAMG;IACH,oBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAInC;IAED;;;;;;OAMG;IACH,oBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAInC;IAED;;;;;;;;OAQG;IACH,kBANW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAKnC;IAED;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAKnC;IAED;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAKnC;IAED;;;;;;;;OAQG;IACH,oBANW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAKnC;IAED;;;;;;;;OAQG;IACH,8BANW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAM3B;IAED;;;;;;;;OAQG;IACH,8BANW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAM3B;IAED;;;;;;;;;OASG;IACH,4BAPW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAM3B;IAED;;;;;;OAMG;IACH,sBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;OAMG;IACH,sBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;;OAOG;IACH,oBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;OAMG;IACH,kCAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;OAMG;IACH,kCAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;;OAOG;IACH,gCALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;OAMG;IACH,6BAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,aAAa,EAAE,CAI3B;IAED;;;;;;OAMG;IACH,6BAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,aAAa,EAAE,CAI3B;IAED;;;;;;;OAOG;IACH,2BALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,aAAa,EAAE,CAI3B;IAED;;;;;;OAMG;IACH,gCAHW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,uBAIhB;IAED;;;;;;OAMG;IACH,gCAHW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,uBAIhB;IAED;;;;;;;OAOG;IACH,8BAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,uBAIhB;;CACF;;;;;yBA79BY,OAAO,eAAe,EAAE,aAAa;mBAIrC,OAAO,eAAe,EAAE,OAAO;;;gBAI/B,OAAO,aAAa,EAAE,QAAQ;;;;AApB3C,8BAAkE;AAmFlE;;;;;;;GAOG;AACH;IAQI,qBAAsC;IAGxC;;;;;OAKG;IACH,sCAEC;IAED;;;OAGG;IACH,8BAEC;CAKF;AAhHD;;;GAGG;AACH;;;GAGG;AACH;;;GAGG;AACH;;;GAGG;AAEH;;;;;GAKG;AACH;IACE;;;;OAIG;IACH,mCAEC;IADC,cAAgC;IAGlC;;;OAGG;IACH,oBAEC;IAED;;;OAGG;IACH,2BAGC;IAED;;;OAGG;IACH,oDAWC;IAED,mBAEC;CACF"}
+{"version":3,"file":"item-persistence.d.ts","sourceRoot":"","sources":["../../src/items/item-persistence.js"],"names":[],"mappings":";AAqJA;;;;;;;;;;;GAWG;AACH;IAKE;;;;OAIG;IACH,8BAKC;IAdD,0BAEC;IADC,aAAsB;IAexB;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;OA+BG;IACH,oBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,UAC3B,MAAM,GAAC,MAAM,GAAC,KAAK,aAAa,GAAC,QAAQ,YAAU,eACnD,gBAAgB,cAChB,MAAM,wBAiChB;IAqDD;;;;;;OAMG;IACH,0BAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,aAAa,GAAG,IAAI,CAAC,CAIlC;IAED;;;;;;MAME;IACF,uBAJW,MAAM,mBACJ,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,CAKvC;IAED;;;;;OAKG;IACH,uBAHW,MAAM,mBACJ,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,CAIvC;IAED;;;;;;OAMG;IACH,uBAJW,MAAM,mBACJ,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,CAKvC;IAED;;;;;OAKG;IACH,uBAHW,MAAM,mBACJ,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,CAIvC;IAED;;;;;;OAMG;IACH,0BAJW,OAAO,cACP,MAAM,mBACJ,CAAC,aAAa,GAAG,IAAI,CAAC,CAIlC;IAED;;;;;;OAMG;IACH,sBAJW,OAAO,cACP,MAAM,mBACJ,CAAC,aAAa,GAAG,IAAI,CAAC,CAIlC;IAED;;;;;;;OAOG;IACH,wBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAKnB;IAED;;;;;;;OAOG;IACH,wBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAKnB;IAED;;;;;;;;OAQG;IACH,sBANW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAKnB;IAED;;;;;;;OAOG;IACH,wBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAKnB;IAED;;;;;;;OAOG;IACH,wBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAKnB;IAED;;;;;;;;OAQG;IACH,sBANW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,OAAO,CAKnB;IAED;;;;;;;OAOG;IACH,wBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,aAAa,GAAG,IAAI,CAAC,CAKlC;IAED;;;;;;;OAOG;IACH,wBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,aAAa,GAAG,IAAI,CAAC,CAKlC;IAED;;;;;;;;OAQG;IACH,sBANW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,aAAa,GAAG,IAAI,CAAC,CAKlC;IAED;;;;;;;OAOG;IACH,wBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,aAAa,GAAG,IAAI,CAAC,CAKlC;IAED;;;;;;;OAOG;IACH,wBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,aAAa,GAAG,IAAI,CAAC,CAKlC;IAED;;;;;;;;OAQG;IACH,sBANW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,aAAa,GAAG,IAAI,CAAC,CAKlC;IAED;;;;;;;;;;;OAWG;IACH,yBATW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,iCAI3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAMnC;IAED;;;;;;;;;;;OAWG;IACH,yBATW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,iCAI3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAMnC;IAED;;;;;;;;;;;;OAYG;IACH,uBAVW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,iCAI3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAMnC;IAED;;;;;;;;;;;OAWG;IACH,0BATW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,iCAI3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAMnC;IAED;;;;;;;;;;;OAWG;IACH,0BATW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,iCAI3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAMnC;IAED;;;;;;;;;;;;OAYG;IACH,wBAVW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,iCAI3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAMnC;IAED;;;;;;;;;;;;;;OAcG;IACH,wBAPW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,iCAI3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAInC;IAED;;;;;;;;;OASG;IACH,wBAPW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,iCAI3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAInC;IAED;;;;;;;;;;OAUG;IACH,sBARW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,iCAI3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAInC;IAED;;;;;;;;;;;;;;OAcG;IACH,2BAPW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,iCAI3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAInC;IAED;;;;;;;;;MASE;IACF,2BAPU,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,iCAI3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAIlC;IAED;;;;;;;;;;MAUE;IACF,yBARU,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,iCAI3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAIlC;IAED;;;;;;;;;;;OAWG;IACH,uBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAInC;IAED;;;;;;OAMG;IACH,uBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAInC;IAED;;;;;;;OAOG;IACH,qBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAInC;IAED;;;;;;OAMG;IACH,oBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAInC;IAED;;;;;;OAMG;IACH,oBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAInC;IAED;;;;;;;;OAQG;IACH,kBANW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAKnC;IAED;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAKnC;IAED;;;;;;;OAOG;IACH,sBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAKnC;IAED;;;;;;;;OAQG;IACH,oBANW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,cAAc,GAAG,IAAI,CAAC,CAKnC;IAED;;;;;;;;OAQG;IACH,8BANW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAM3B;IAED;;;;;;;;OAQG;IACH,8BANW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAM3B;IAED;;;;;;;;;OASG;IACH,4BAPW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,CAAC,MAAM,GAAG,IAAI,CAAC,CAM3B;IAED;;;;;;OAMG;IACH,sBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;OAMG;IACH,sBAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;;OAOG;IACH,oBALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;OAMG;IACH,kCAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;OAMG;IACH,kCAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;;OAOG;IACH,gCALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,MAAM,CAIlB;IAED;;;;;;OAMG;IACH,6BAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,aAAa,EAAE,CAI3B;IAED;;;;;;OAMG;IACH,6BAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,aAAa,EAAE,CAI3B;IAED;;;;;;;OAOG;IACH,2BALW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,mBACJ,aAAa,EAAE,CAI3B;IAED;;;;;;OAMG;IACH,gCAHW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,uBAIhB;IAED;;;;;;OAMG;IACH,gCAHW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,uBAIhB;IAED;;;;;;;OAOG;IACH,8BAJW,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,OAC3B,CAAC,KAAK,aAAa,GAAG,IAAI,CAAC,cAC3B,MAAM,uBAIhB;;CACF;;;;;yBAn9BY,OAAO,eAAe,EAAE,aAAa;mBAIrC,OAAO,eAAe,EAAE,OAAO;;;gBAI/B,OAAO,aAAa,EAAE,QAAQ;;;;AApB3C,8BAAkE;AAyElE;;;;;;;GAOG;AACH;IAQI,qBAAsC;IAGxC;;;;;OAKG;IACH,sCAEC;IAED;;;OAGG;IACH,8BAEC;CAKF;AAtGD;;;GAGG;AACH;;;GAGG;AACH;;;GAGG;AACH;;;GAGG;AAEH;;;;;GAKG;AACH;IACE;;;;OAIG;IACH,mCAEC;IADC,cAAgC;IAGlC;;;OAGG;IACH,oBAEC;IAED;;;OAGG;IACH,2BAEC;IAED;;;OAGG;IACH,oDAEC;IAED,mBAEC;CACF"}

types/items/items.d.ts.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"items.d.ts","sourceRoot":"","sources":["../../src/items/items.js"],"names":[],"mappings":";;;;;;;UA0Bc,MAAM;;;;UACN,MAAM;;;;YACN,MAAM;;;;eACN,MAAM;;;;aACN,MAAM,EAAE;;;;WACR,MAAM,EAAE;;;;eACR,MAAM,MAAO;;;;;;;;iBAEb,MAAM;;;;;;;;;;4BAQP,OAAO,eAAe,EAAE,aAAa;sBAIrC,OAAO,eAAe,EAAE,OAAO;uBAI/B,OAAO,eAAe,EAAE,QAAQ;uBAIhC,OAAO,aAAa,EAAE,QAAQ;AAgwB3C;;;;;GAKG;AACH,gCAHW,MAAM,GACJ,MAAM,CAIa;AA/DhC;;;;;GAKG;AACH,iCAHW,MAAM,GACJ,OAAO,CASnB;AAED;;;;;;GAMG;AACH,8BAJW,MAAM,kBACN,OAAO,GACL,IAAI,CAYhB;AAED;;;;;GAKG;AACH,4BAFa,IAAI,EAAE,CAIlB;AAzJD;;;;;;;;;;;GAWG;AACH,oCALW,UAAU,GACR,IAAI,CA+BhB;AAoHD;;;;;;GAMG;AACH,2CAHW,MAAM,EAAE,GACN,IAAI,EAAE,CAIlB;AA7ED;;;;;;;;;;;;;;GAcG;AACH,wCALW,UAAU,GACR,IAAI,GAAC,IAAI,CAWrB;AApED;;;;;;GAMG;AACH,2CAHW,MAAM,GAAC,IAAI,GACT,IAAI,GAAC,IAAI,CAuCrB;AApqBD;;;;GAIG;AACH;IACE;;;;OAIG;IACH,+BAqBC;IAjBC;;;OAGG;IACH,kBAAsB;IAEtB;;;OAGG;IACH,aAFU,eAAe,CAEsB;IAE/C;;;OAGG;IACH,WAFU,aAAa,CAE6B;IAGtD;;;OAGG;IACH,mBAEC;IAED;;;OAGG;IACH,mBAEC;IAED;;;OAGG;IACH,oBAEC;IAED;;;OAGG;IACH,oBAEC;IAED;;;OAGG;IACH,2BAEC;IAED;;;OAGG;IACH,oDAEC;IAED;;;OAGG;IACH,0BAEC;IAED;;;OAGG;IACH,4BAEC;IAED;;;OAGG;IACH,mCAEC;IAED;;;OAGG;IACH,4DAEC;IAED;;;OAGG;IACH,4BAEC;IAED;;;OAGG;IACH,oCAIC;IAED;;;OAGG;IACH,kCAIC;IAED;;;OAGG;IACH,oCAIC;IAED;;;OAGG;IACH,kCAIC;IAED;;;OAGG;IACH,sBAEC;IAED;;;OAGG;IACH,0BAEC;IAED;;;OAGG;IACH,+BAKC;IAED;;;;;;;;;;;;;OAaG;IACH,wBAHW,MAAM,GACJ;QAAE,SAAS,EAAE,YAAY,CAAA;KAAE,GAAC,YAAY,GAAC,IAAI,CAIzD;IAED;;;;;;;;OAQG;IACH,2BALW,MAAM,SACN,MAAM,kBACN,MAAM,GACJ;QAAC,aAAa,MAAI;QAAC,KAAK,EAAE,MAAM,CAAA;KAAC,GAAC,IAAI,CAIlD;IAED;;;;;;OAMG;IACH,2BAHW,MAAM,GACJ,YAAY,GAAC,IAAI,CAI7B;IAED;;;;;;;;;;;;;;;;OAgBG;IACH,mBANW,MAAM,GAAC,MAAM,GAAC,aAAa,GAAC,OAAO,GAAC,QAAQ,YAAU,WACtD,QAAQ,aACR,MAAM,GAAC,MAAM,GAAC,aAAa,GAAC,OAAO,GAAC,QAAQ,YAAU,QAoBhE;IAED;;;;;;OAMG;IACH,8BAJW,MAAM,GAAC,MAAM,GAAC,aAAa,GAAC,OAAO,GAAC,QAAQ,YAAU,GACpD,OAAO,CAkCnB;IAED;;;;;OAKG;IACH,2BAHW,MAAM,GAAC,QAAQ,YAAU,GACxB,OAAO,CAuBlB;IAED;;;;;OAKG;IACH,2BAHW,MAAM,GAAC,QAAQ,YAAU,GACxB,OAAO,CAuBlB;IAED;;;;;;;OAOG;IACH,sEAkBC;IAED;;;OAGG;IACH,0BAKC;IAED;;;;OAIG;IACH,yBAEC;IAED;;;;;;OAMG;IACH,kBAJW,MAAM,GAAC,MAAM,GAAC,aAAa,GAAC,OAAO,GAAC,QAAQ,YAAU,QAMhE;IAED;;;OAGG;IACH,2BAEC;IAED;;;OAGG;IACH,6CAIC;IAED;;;OAGG;IACH,gDAMC;IAED;;;OAGG;IACH,qBAEC;IAED;;;OAGG;IACH,qBAFc,MAAM,UAKnB;IAED;;;OAGG;IACH,wBAFc,MAAM,UAOnB;IAED,gBAEC;CACF"}
+{"version":3,"file":"items.d.ts","sourceRoot":"","sources":["../../src/items/items.js"],"names":[],"mappings":";;;;;;;UA0Bc,MAAM;;;;UACN,MAAM;;;;YACN,MAAM;;;;eACN,MAAM;;;;aACN,MAAM,EAAE;;;;WACR,MAAM,EAAE;;;;eACR,MAAM,MAAO;;;;;;;;iBAEb,MAAM;;;;;;;;;;4BAQP,OAAO,eAAe,EAAE,aAAa;sBAIrC,OAAO,eAAe,EAAE,OAAO;uBAI/B,OAAO,eAAe,EAAE,QAAQ;uBAIhC,OAAO,aAAa,EAAE,QAAQ;AA8sB3C;;;;;GAKG;AACH,gCAHW,MAAM,GACJ,MAAM,CAIa;AA/DhC;;;;;GAKG;AACH,iCAHW,MAAM,GACJ,OAAO,CASnB;AAED;;;;;;GAMG;AACH,8BAJW,MAAM,kBACN,OAAO,GACL,IAAI,CAYhB;AAED;;;;;GAKG;AACH,4BAFa,IAAI,EAAE,CAIlB;AAzJD;;;;;;;;;;;GAWG;AACH,oCALW,UAAU,GACR,IAAI,CA+BhB;AAoHD;;;;;;GAMG;AACH,2CAHW,MAAM,EAAE,GACN,IAAI,EAAE,CAIlB;AA7ED;;;;;;;;;;;;;;GAcG;AACH,wCALW,UAAU,GACR,IAAI,GAAC,IAAI,CAWrB;AApED;;;;;;GAMG;AACH,2CAHW,MAAM,GAAC,IAAI,GACT,IAAI,GAAC,IAAI,CAuCrB;AAlnBD;;;;GAIG;AACH;IACE;;;;OAIG;IACH,+BAqBC;IAjBC;;;OAGG;IACH,kBAAsB;IAEtB;;;OAGG;IACH,aAFU,eAAe,CAEsB;IAE/C;;;OAGG;IACH,WAFU,aAAa,CAE6B;IAGtD;;;OAGG;IACH,mBAEC;IAED;;;OAGG;IACH,mBAEC;IAED;;;OAGG;IACH,oBAEC;IAED;;;OAGG;IACH,oBAEC;IAED;;;OAGG;IACH,2BAEC;IAED;;;OAGG;IACH,oDAEC;IAED;;;OAGG;IACH,0BAEC;IAED;;;OAGG;IACH,4BAEC;IAED;;;OAGG;IACH,mCAEC;IAED;;;OAGG;IACH,4DAEC;IAED;;;OAGG;IACH,4BAEC;IAED;;;OAGG;IACH,oCAIC;IAED;;;OAGG;IACH,kCAIC;IAED;;;OAGG;IACH,oCAIC;IAED;;;OAGG;IACH,kCAIC;IAED;;;OAGG;IACH,sBAEC;IAED;;;OAGG;IACH,0BAEC;IAED;;;OAGG;IACH,+BAKC;IAED;;;;;;;;;;;;;OAaG;IACH,wBAHW,MAAM,GACJ;QAAE,SAAS,EAAE,YAAY,CAAA;KAAE,GAAC,YAAY,GAAC,IAAI,CAIzD;IAED;;;;;;;;OAQG;IACH,2BALW,MAAM,SACN,MAAM,kBACN,MAAM,GACJ;QAAC,aAAa,MAAI;QAAC,KAAK,EAAE,MAAM,CAAA;KAAC,GAAC,IAAI,CAIlD;IAED;;;;;;OAMG;IACH,2BAHW,MAAM,GACJ,YAAY,GAAC,IAAI,CAI7B;IAED;;;;;;;;;;;;;;;;OAgBG;IACH,mBANW,MAAM,GAAC,MAAM,GAAC,aAAa,GAAC,OAAO,GAAC,QAAQ,YAAU,WACtD,QAAQ,aACR,MAAM,GAAC,MAAM,GAAC,aAAa,GAAC,OAAO,GAAC,QAAQ,YAAU,QAoBhE;IAED;;;;;;OAMG;IACH,8BAJW,MAAM,GAAC,MAAM,GAAC,aAAa,GAAC,OAAO,GAAC,QAAQ,YAAU,GACpD,OAAO,CAkCnB;IAED;;;;;OAKG;IACH,2BAHW,MAAM,GAAC,QAAQ,YAAU,GACxB,OAAO,CAuBlB;IAED;;;;;OAKG;IACH,2BAHW,MAAM,GAAC,QAAQ,YAAU,GACxB,OAAO,CAuBlB;IAED;;;;;;;OAOG;IACH,sEAkBC;IAED;;;OAGG;IACH,0BAKC;IAED;;;;OAIG;IACH,yBAEC;IAED;;;;;;OAMG;IACH,kBAJW,MAAM,GAAC,MAAM,GAAC,aAAa,GAAC,OAAO,GAAC,QAAQ,YAAU,QAMhE;IAED;;;OAGG;IACH,2BAEC;IAED;;;OAGG;IACH,6CAIC;IAED;;;OAGG;IACH,gDAMC;IAED;;;OAGG;IACH,qBAEC;IAED;;;OAGG;IACH,qBAFc,MAAM,UAKnB;IAED;;;OAGG;IACH,wBAFc,MAAM,UAOnB;IAED,gBAEC;CACF"}

types/rules/operation-builder.d.ts
@@ -8,6 +8,12 @@ export type Item = {
     readonly state: string;
     readonly numericState: number;
     readonly quantityState: import("../quantity").Quantity;
+    /**
+       * Sends the state from one item to another. Can be used to proxy item state. State is
+       * sent as a command.
+       *
+       * @returns {CopyStateOperation} the operation config
+       */
     readonly rawState: HostState;
     readonly previousState: string;
     readonly previousNumericState: number;
@@ -36,7 +42,7 @@ export type Item = {
     removeMetadata(namespace?: string): {
         value: string;
         configuration: any;
-    };
+    }; /** @private */
     sendCommand(value: any, expire?: time.Duration, onExpire?: any): void;
     sendCommandIfDifferent(value: any): boolean;
     sendIncreaseCommand(value: any): boolean;

commit 7b27863588577c43b782b422f84e4c963cad3ded

[items] ItemPersistence: Fix Quantity state missing
Regression from #437.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

src/items/item-persistence.js
@@ -69,7 +69,7 @@ class PersistedState {
    * @type {Quantity|null}
    */
   get quantityState () {
-    _quantityStateOrNull(this.rawState);
+    return _quantityStateOrNull(this.rawState);
   }
 
   toString () {