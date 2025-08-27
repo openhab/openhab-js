openhab/openhab-js

commit a539e28657c1b1b06aff5ad78c6c28ebf9c6075d

[items] Fix NULL Item state not recognized as null (#448)
Fixes #446.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

src/items/helpers.js
@@ -1,8 +1,19 @@
 // Helper functions used internally across the items namespace
 
-const { PercentType } = require('@runtime');
+const { UnDefType, PercentType } = require('@runtime');
 
 const { getQuantity, QuantityError } = require('../quantity');
+
+/**
+ * Check if a value is `null`, `undefined` or an instance of {@link https://www.openhab.org/javadoc/latest/org/openhab/core/types/undeftype `UnDefType`}.
+ *
+ * @private
+ * @param {*} value
+ * @returns {boolean}
+ */
+function _isNullOrUndefined (value) {
+  return value === null || value === undefined || value instanceof UnDefType;
+}
 
 /**
  * Return a string representation of a state.
@@ -12,7 +23,7 @@ const { getQuantity, QuantityError } = require('../quantity');
  * @returns {string|null} string representation or `null` if `rawState` was `null`
  */
 function _stateOrNull (rawState) {
-  if (rawState === null) return null;
+  if (_isNullOrUndefined(rawState)) return null;
   return rawState.toString();
 }
 
@@ -25,7 +36,7 @@ function _stateOrNull (rawState) {
  * @returns {number|null} numeric representation or `null` if `rawState` was `null`
  */
 function _numericStateOrNull (rawState, type) {
-  if (rawState === null) return null;
+  if (_isNullOrUndefined(rawState)) return null;
   let state = rawState.toString();
   if (type === 'Color') state = rawState.as(PercentType).toString();
   const numericState = parseFloat(state);
@@ -41,7 +52,7 @@ function _numericStateOrNull (rawState, type) {
  * @throws failed to create quantityState
  */
 function _quantityStateOrNull (rawState) {
-  if (rawState === null) return null;
+  if (_isNullOrUndefined(rawState)) return null;
   try {
     const qty = getQuantity(rawState);
     return (qty !== null && qty.symbol !== null) ? qty : null;
@@ -55,6 +66,7 @@ function _quantityStateOrNull (rawState) {
 }
 
 module.exports = {
+  _isNullOrUndefined,
   _stateOrNull,
   _numericStateOrNull,
   _quantityStateOrNull

src/items/items.js
@@ -12,12 +12,12 @@ const cache = require('../cache');
 const time = require('../time');
 const environment = require('../environment');
 
-const { OnOffType, UnDefType, events } = require('@runtime');
+const { OnOffType, events } = require('@runtime');
 const itemRegistry = environment.useProviderRegistries()
   ? require('@runtime/provider').itemRegistry
   : require('@runtime').itemRegistry;
 
-const { _stateOrNull, _numericStateOrNull, _quantityStateOrNull } = require('./helpers');
+const { _stateOrNull, _numericStateOrNull, _quantityStateOrNull, _isNullOrUndefined } = require('./helpers');
 const metadata = require('./metadata');
 const itemChannelLink = require('./itemchannellink');
 const ItemPersistence = require('./item-persistence');
@@ -251,10 +251,7 @@ class Item {
    * @type {boolean}
    */
   get isUninitialized () {
-    return (this.rawItem.getState() instanceof UnDefType ||
-        this.rawItem.getState().toString() === 'Undefined' ||
-        this.rawItem.getState().toString() === 'Uninitialized'
-    );
+    return _isNullOrUndefined(this.rawState);
   }
 
   /**

src/things.js
@@ -145,10 +145,6 @@ class Thing {
   }
 }
 
-function addThing () {
-
-}
-
 /**
  * Gets an openHAB Thing.
  * Returns `null` if no Thing with the given UID exists.
@@ -175,7 +171,6 @@ function getThings () {
 
 module.exports = {
   Thing,
-  addThing,
   getThing,
   getThings
 };

types/items/items.d.ts.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"items.d.ts","sourceRoot":"","sources":["../../src/items/items.js"],"names":[],"mappings":";;;;;;;UA+Bc,MAAM;;;;UACN,MAAM;;;;YACN,MAAM;;;;eACN,MAAM;;;;aACN,MAAM,EAAE;;;;WACR,MAAM,EAAE;;;;eACR,MAAM,MAAO;;;;;;;;iBAEb,MAAM;;;;;;;;;;4BAQP,OAAO,eAAe,EAAE,aAAa;sBAIrC,OAAO,eAAe,EAAE,OAAO;uBAI/B,OAAO,eAAe,EAAE,QAAQ;uBAIhC,OAAO,aAAa,EAAE,QAAQ;AAksB3C;;;;;GAKG;AACH,gCAHW,MAAM,GACJ,MAAM,CAIa;AA/DhC;;;;;GAKG;AACH,iCAHW,MAAM,GACJ,OAAO,CASnB;AAED;;;;;;GAMG;AACH,8BAJW,MAAM,kBACN,OAAO,GACL,IAAI,CAYhB;AAED;;;;;GAKG;AACH,4BAFa,IAAI,EAAE,CAIlB;AAhJD;;;;;;;;;;;;;;;;GAgBG;AACH,oCANW,UAAU,YACV,OAAO,GACL,IAAI,CA+ChB;AAsFD;;;;;;GAMG;AACH,2CAHW,MAAM,EAAE,GACN,IAAI,EAAE,CAIlB;AA/ED;;;;;;;;;;;;;;;;GAgBG;AACH,wCALW,UAAU,GACR,IAAI,GAAC,IAAI,CAWrB;AAtCD;;;;;;GAMG;AACH,2CAHW,MAAM,GAAC,IAAI,GACT,IAAI,GAAC,IAAI,CAOrB;AApmBD;;;;GAIG;AACH;IACE;;;;OAIG;IACH,+BAqBC;IAjBC;;;OAGG;IACH,kBAAsB;IAEtB;;;OAGG;IACH,aAFU,eAAe,CAEsB;IAE/C;;;OAGG;IACH,WAFU,aAAa,CAE6B;IAGtD;;;OAGG;IACH,mBAEC;IAED;;;OAGG;IACH,mBAEC;IAED;;;OAGG;IACH,oBAEC;IAED;;;OAGG;IACH,oBAEC;IAED;;;OAGG;IACH,2BAEC;IAED;;;OAGG;IACH,oDAEC;IAED;;;OAGG;IACH,0BAEC;IAED;;;OAGG;IACH,4BAEC;IAED;;;OAGG;IACH,mCAEC;IAED;;;OAGG;IACH,4DAEC;IAED;;;OAGG;IACH,4BAEC;IAED;;;OAGG;IACH,oCAIC;IAED;;;OAGG;IACH,kCAIC;IAED;;;OAGG;IACH,oCAIC;IAED;;;OAGG;IACH,kCAIC;IAED;;;OAGG;IACH,sBAEC;IAED;;;OAGG;IACH,0BAEC;IAED;;;OAGG;IACH,+BAKC;IAED;;;;;;;;;;;;;OAaG;IACH,wBAHW,MAAM,GACJ;QAAE,SAAS,EAAE,YAAY,CAAA;KAAE,GAAC,YAAY,GAAC,IAAI,CAIzD;IAED;;;;;;;;;;OAUG;IACH,2BALW,MAAM,SACN,MAAM,kBACN,MAAM,GACJ,YAAY,GAAC,IAAI,CAI7B;IAED;;;;;;OAMG;IACH,2BAHW,MAAM,GACJ,YAAY,GAAC,IAAI,CAI7B;IAED;;;;;;;;;;;;;;;;OAgBG;IACH,mBANW,MAAM,GAAC,MAAM,GAAC,aAAa,GAAC,OAAO,GAAC,QAAQ,YAAU,WACtD,QAAQ,aACR,MAAM,GAAC,MAAM,GAAC,aAAa,GAAC,OAAO,GAAC,QAAQ,YAAU,QAoBhE;IAED;;;;;;OAMG;IACH,8BAJW,MAAM,GAAC,MAAM,GAAC,aAAa,GAAC,OAAO,GAAC,QAAQ,YAAU,GACpD,OAAO,CAkCnB;IAED;;;;;OAKG;IACH,2BAHW,MAAM,GAAC,QAAQ,YAAU,GACxB,OAAO,CAuBlB;IAED;;;;;OAKG;IACH,2BAHW,MAAM,GAAC,QAAQ,YAAU,GACxB,OAAO,CAuBlB;IAED;;;;;;;OAOG;IACH,sEAkBC;IAED;;;OAGG;IACH,0BAKC;IAED;;;;OAIG;IACH,yBAEC;IAED;;;;;;OAMG;IACH,kBAJW,MAAM,GAAC,MAAM,GAAC,aAAa,GAAC,OAAO,GAAC,QAAQ,YAAU,QAMhE;IAED;;;OAGG;IACH,2BAEC;IAED;;;OAGG;IACH,6CAIC;IAED;;;OAGG;IACH,gDAMC;IAED;;;OAGG;IACH,qBAEC;IAED;;;OAGG;IACH,qBAFc,MAAM,UAKnB;IAED;;;OAGG;IACH,wBAFc,MAAM,UAOnB;IAED,gBAEC;CACF"}
+{"version":3,"file":"items.d.ts","sourceRoot":"","sources":["../../src/items/items.js"],"names":[],"mappings":";;;;;;;UA+Bc,MAAM;;;;UACN,MAAM;;;;YACN,MAAM;;;;eACN,MAAM;;;;aACN,MAAM,EAAE;;;;WACR,MAAM,EAAE;;;;eACR,MAAM,MAAO;;;;;;;;iBAEb,MAAM;;;;;;;;;;4BAQP,OAAO,eAAe,EAAE,aAAa;sBAIrC,OAAO,eAAe,EAAE,OAAO;uBAI/B,OAAO,eAAe,EAAE,QAAQ;uBAIhC,OAAO,aAAa,EAAE,QAAQ;AA+rB3C;;;;;GAKG;AACH,gCAHW,MAAM,GACJ,MAAM,CAIa;AA/DhC;;;;;GAKG;AACH,iCAHW,MAAM,GACJ,OAAO,CASnB;AAED;;;;;;GAMG;AACH,8BAJW,MAAM,kBACN,OAAO,GACL,IAAI,CAYhB;AAED;;;;;GAKG;AACH,4BAFa,IAAI,EAAE,CAIlB;AAhJD;;;;;;;;;;;;;;;;GAgBG;AACH,oCANW,UAAU,YACV,OAAO,GACL,IAAI,CA+ChB;AAsFD;;;;;;GAMG;AACH,2CAHW,MAAM,EAAE,GACN,IAAI,EAAE,CAIlB;AA/ED;;;;;;;;;;;;;;;;GAgBG;AACH,wCALW,UAAU,GACR,IAAI,GAAC,IAAI,CAWrB;AAtCD;;;;;;GAMG;AACH,2CAHW,MAAM,GAAC,IAAI,GACT,IAAI,GAAC,IAAI,CAOrB;AAjmBD;;;;GAIG;AACH;IACE;;;;OAIG;IACH,+BAqBC;IAjBC;;;OAGG;IACH,kBAAsB;IAEtB;;;OAGG;IACH,aAFU,eAAe,CAEsB;IAE/C;;;OAGG;IACH,WAFU,aAAa,CAE6B;IAGtD;;;OAGG;IACH,mBAEC;IAED;;;OAGG;IACH,mBAEC;IAED;;;OAGG;IACH,oBAEC;IAED;;;OAGG;IACH,oBAEC;IAED;;;OAGG;IACH,2BAEC;IAED;;;OAGG;IACH,oDAEC;IAED;;;OAGG;IACH,0BAEC;IAED;;;OAGG;IACH,4BAEC;IAED;;;OAGG;IACH,mCAEC;IAED;;;OAGG;IACH,4DAEC;IAED;;;OAGG;IACH,4BAEC;IAED;;;OAGG;IACH,oCAIC;IAED;;;OAGG;IACH,kCAIC;IAED;;;OAGG;IACH,oCAIC;IAED;;;OAGG;IACH,kCAIC;IAED;;;OAGG;IACH,sBAEC;IAED;;;OAGG;IACH,0BAEC;IAED;;;OAGG;IACH,+BAEC;IAED;;;;;;;;;;;;;OAaG;IACH,wBAHW,MAAM,GACJ;QAAE,SAAS,EAAE,YAAY,CAAA;KAAE,GAAC,YAAY,GAAC,IAAI,CAIzD;IAED;;;;;;;;;;OAUG;IACH,2BALW,MAAM,SACN,MAAM,kBACN,MAAM,GACJ,YAAY,GAAC,IAAI,CAI7B;IAED;;;;;;OAMG;IACH,2BAHW,MAAM,GACJ,YAAY,GAAC,IAAI,CAI7B;IAED;;;;;;;;;;;;;;;;OAgBG;IACH,mBANW,MAAM,GAAC,MAAM,GAAC,aAAa,GAAC,OAAO,GAAC,QAAQ,YAAU,WACtD,QAAQ,aACR,MAAM,GAAC,MAAM,GAAC,aAAa,GAAC,OAAO,GAAC,QAAQ,YAAU,QAoBhE;IAED;;;;;;OAMG;IACH,8BAJW,MAAM,GAAC,MAAM,GAAC,aAAa,GAAC,OAAO,GAAC,QAAQ,YAAU,GACpD,OAAO,CAkCnB;IAED;;;;;OAKG;IACH,2BAHW,MAAM,GAAC,QAAQ,YAAU,GACxB,OAAO,CAuBlB;IAED;;;;;OAKG;IACH,2BAHW,MAAM,GAAC,QAAQ,YAAU,GACxB,OAAO,CAuBlB;IAED;;;;;;;OAOG;IACH,sEAkBC;IAED;;;OAGG;IACH,0BAKC;IAED;;;;OAIG;IACH,yBAEC;IAED;;;;;;OAMG;IACH,kBAJW,MAAM,GAAC,MAAM,GAAC,aAAa,GAAC,OAAO,GAAC,QAAQ,YAAU,QAMhE;IAED;;;OAGG;IACH,2BAEC;IAED;;;OAGG;IACH,6CAIC;IAED;;;OAGG;IACH,gDAMC;IAED;;;OAGG;IACH,qBAEC;IAED;;;OAGG;IACH,qBAFc,MAAM,UAKnB;IAED;;;OAGG;IACH,wBAFc,MAAM,UAOnB;IAED,gBAEC;CACF"}

types/quantity.d.ts
@@ -2,8 +2,15 @@ export type Item = {
     rawItem: HostItem;
     persistence: import("./items/item-persistence");
     semantics: import("./items/item-semantics");
-    readonly type: string;
+    readonly type: string; /**
+     * @type {QuantityType}
+     * @private
+     */
     readonly name: string;
+    /**
+     * @type {QuantityType}
+     * @private
+     */
     readonly label: string;
     readonly state: string;
     readonly numericState: number;

types/quantity.d.ts.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"quantity.d.ts","sourceRoot":"","sources":["../src/quantity.js"],"names":[],"mappings":";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;AAqSA;;;;;;;;GAQG;AACH,mCALW,IAAI,GAAC,MAAM,GAAC,QAAQ,MAAa,GAC/B,QAAQ,CAMpB;AAvND;;;;;;;GAOG;AACH;IACE;;OAEG;IACH,mBAFW,IAAI,GAAC,MAAM,GAAC,QAAQ,MAAa,EAgB3C;IAZG;;;OAGG;IACH,mBAAuB;IAU3B;;;OAGG;IACH,wBAEC;IAED;;;OAGG;IACH,mBAGC;IAED;;;OAGG;IACH,qBAIC;IAED;;;OAGG;IACH,oBAEC;IAED;;;OAGG;IACH,kBAEC;IAED;;;;;OAKG;IACH,WAHW,IAAI,GAAC,MAAM,GAAC,QAAQ,GAClB,QAAQ,CAKpB;IAED;;;;;;;;;OASG;IACH,cAHW,IAAI,GAAC,MAAM,GAAC,MAAM,GAAC,QAAQ,GACzB,QAAQ,CAKpB;IAED;;;;;;;;;OASG;IACH,gBAHW,IAAI,GAAC,MAAM,GAAC,MAAM,GAAC,QAAQ,GACzB,QAAQ,CAKpB;IAED;;;;;OAKG;IACH,gBAHW,IAAI,GAAC,MAAM,GAAC,QAAQ,GAClB,QAAQ,CAKpB;IAED;;;;;;OAMG;IACH,aAJW,MAAM,GACJ,QAAQ,GAAC,IAAI,CAezB;IAED;;;;;OAKG;IACH,aAHW,IAAI,GAAC,MAAM,GAAC,QAAQ,GAClB,OAAO,CAKnB;IAED;;;;;OAKG;IACH,mBAHW,IAAI,GAAC,MAAM,GAAC,QAAQ,GAClB,OAAO,CAKnB;IAED;;;;;OAKG;IACH,0BAHW,IAAI,GAAC,MAAM,GAAC,QAAQ,GAClB,OAAO,CAKnB;IAED;;;;;OAKG;IACH,gBAHW,IAAI,GAAC,MAAM,GAAC,QAAQ,GAClB,OAAO,CAKnB;IAED;;;;;OAKG;IACH,uBAHW,IAAI,GAAC,MAAM,GAAC,QAAQ,GAClB,OAAO,CAKnB;IAED,gBAEC;CACF;AAxND;;;GAGG;AACH;IACE;;OAEG;IACH,qBAFW,MAAM,EAKhB;CACF;AA9CD;;;;;;;;GAQG;AACH,kCAPW,IAAI,GAAC,MAAM,GAAC,QAAQ,aACpB,MAAM,OA6BhB;AAjED;;;GAGG;AAEH;;;;;;;;GAQG;AACH,8CANW,IAAI,GAAC,MAAM,GAAC,MAAM,GAAC,QAAQ,GACzB,oBAAuB,CAsBnC"}
+{"version":3,"file":"quantity.d.ts","sourceRoot":"","sources":["../src/quantity.js"],"names":[],"mappings":";;;;2BAuGM;;;OAGG;;IAGH;;;OAGG;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;AAqLT;;;;;;;;GAQG;AACH,mCALW,IAAI,GAAC,MAAM,GAAC,QAAQ,MAAa,GAC/B,QAAQ,CAMpB;AAvND;;;;;;;GAOG;AACH;IACE;;OAEG;IACH,mBAFW,IAAI,GAAC,MAAM,GAAC,QAAQ,MAAa,EAgB3C;IAZG;;;OAGG;IACH,mBAAuB;IAU3B;;;OAGG;IACH,wBAEC;IAED;;;OAGG;IACH,mBAGC;IAED;;;OAGG;IACH,qBAIC;IAED;;;OAGG;IACH,oBAEC;IAED;;;OAGG;IACH,kBAEC;IAED;;;;;OAKG;IACH,WAHW,IAAI,GAAC,MAAM,GAAC,QAAQ,GAClB,QAAQ,CAKpB;IAED;;;;;;;;;OASG;IACH,cAHW,IAAI,GAAC,MAAM,GAAC,MAAM,GAAC,QAAQ,GACzB,QAAQ,CAKpB;IAED;;;;;;;;;OASG;IACH,gBAHW,IAAI,GAAC,MAAM,GAAC,MAAM,GAAC,QAAQ,GACzB,QAAQ,CAKpB;IAED;;;;;OAKG;IACH,gBAHW,IAAI,GAAC,MAAM,GAAC,QAAQ,GAClB,QAAQ,CAKpB;IAED;;;;;;OAMG;IACH,aAJW,MAAM,GACJ,QAAQ,GAAC,IAAI,CAezB;IAED;;;;;OAKG;IACH,aAHW,IAAI,GAAC,MAAM,GAAC,QAAQ,GAClB,OAAO,CAKnB;IAED;;;;;OAKG;IACH,mBAHW,IAAI,GAAC,MAAM,GAAC,QAAQ,GAClB,OAAO,CAKnB;IAED;;;;;OAKG;IACH,0BAHW,IAAI,GAAC,MAAM,GAAC,QAAQ,GAClB,OAAO,CAKnB;IAED;;;;;OAKG;IACH,gBAHW,IAAI,GAAC,MAAM,GAAC,QAAQ,GAClB,OAAO,CAKnB;IAED;;;;;OAKG;IACH,uBAHW,IAAI,GAAC,MAAM,GAAC,QAAQ,GAClB,OAAO,CAKnB;IAED,gBAEC;CACF;AAxND;;;GAGG;AACH;IACE;;OAEG;IACH,qBAFW,MAAM,EAKhB;CACF;AA9CD;;;;;;;;GAQG;AACH,kCAPW,IAAI,GAAC,MAAM,GAAC,QAAQ,aACpB,MAAM,OA6BhB;AAjED;;;GAGG;AAEH;;;;;;;;GAQG;AACH,8CANW,IAAI,GAAC,MAAM,GAAC,MAAM,GAAC,QAAQ,GACzB,oBAAuB,CAsBnC"}

commit 6f5c529f8d74733ea7f324aaa73b45fbb8eb7d65

[items] Item: Fix .state returns null if state is NULL or UNDEF
Regression from #448.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

src/items/helpers.js
@@ -23,7 +23,7 @@ function _isNullOrUndefined (value) {
  * @returns {string|null} string representation or `null` if `rawState` was `null`
  */
 function _stateOrNull (rawState) {
-  if (_isNullOrUndefined(rawState)) return null;
+  if (rawState === null) return null; // check for passed in null only, allows returning NULL or UNDEF
   return rawState.toString();
 }
 