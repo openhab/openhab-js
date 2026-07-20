b646f391d4671269f976a7334295bbab0ecb3646
Ravi Nadahar <nadahar@rediffmail.com>
2026-07-20 19:29:42 +0200

    Tweak JSDocs
    Signed-off-by: Ravi Nadahar <nadahar@rediffmail.com>

--- types/cache.d.ts
+++ types/cache.d.ts
@@ -11,8 +11,8 @@ export class JSCache {
      * Returns the value to which the specified key is mapped.
      *
      * @param {string} key the key whose associated value is to be returned
-     * @param {function} [defaultSupplier] if the specified key is not already associated with a value, this function will return a default value
-     * @param {boolean} [jsifyResult=true] whether or not the result will be automatically converted to JavaScript objects when possible.
+     * @param {function} [defaultSupplier] if the specified key is not already associated with a value, this function will return a default value. The output of the function will be run through javaify() if the cache is shared, since the shared cache can't contain JavaScript objects.
+     * @param {boolean} [jsifyResult=true] whether or not the result will be automatically converted to JavaScript objects when possible
      * @returns {*|null} the current object for the supplied key, the value returned by defaultSupplier (if provided), or `null`
      */
     get(key: string, defaultSupplier?: Function, jsifyResult?: boolean): any | null;
@@ -20,8 +20,8 @@ export class JSCache {
      * Associates the specified value with the specified key.
      *
      * @param {string} key key with which the specified value is to be associated
-     * @param {*} value value to be associated with the specified key
-     * @param {boolean} [jsifyResult=true] whether or not the result will be automatically converted to JavaScript objects when possible.
+     * @param {*} value value to be associated with the specified key. The value will be run through javaify() if the cache is shared, since the shared cache can't contain JavaScript objects.
+     * @param {boolean} [jsifyResult=true] whether or not the result will be automatically converted to JavaScript objects when possible
      * @returns {*|null} the previous value associated with the key, or null if there was no mapping for key
      */
     put(key: string, value: any, jsifyResult?: boolean): any | null;
@@ -29,7 +29,7 @@ export class JSCache {
      * Removes the mapping for a key from this map if it is present.
      *
      * @param {string} key key whose mapping is to be removed from the cache
-     * @param {boolean} [jsifyResult=true] whether or not the result will be automatically converted to JavaScript objects when possible.
+     * @param {boolean} [jsifyResult=true] whether or not the result will be automatically converted to JavaScript objects when possible
      * @returns {*|null} the previous value associated with the key or null if there was no mapping for key
      */
     remove(key: string, jsifyResult?: boolean): any | null;
